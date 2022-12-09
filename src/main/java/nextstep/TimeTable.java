0a32bbd2238a84eea2dabf8a0d970fe93ccd4361
boorownie <probitanima11@gmail.com>
2022-12-10 08:31:49 +0900

    default code

---
+++ src/main/java/nextstep/TimeTable.java
@@ -0,0 +1,20 @@
+package nextstep;
+
+import java.time.LocalTime;
+
+public enum TimeTable {
+    A(LocalTime.of(11, 30)),
+    B(LocalTime.of(13, 00)),
+    C(LocalTime.of(14, 30)),
+    D(LocalTime.of(16, 00)),
+    E(LocalTime.of(17, 30)),
+    F(LocalTime.of(19, 00)),
+    G(LocalTime.of(20, 30)),
+    H(LocalTime.of(22, 00));
+
+    private LocalTime time;
+
+    TimeTable(LocalTime time) {
+        this.time = time;
+    }
+}
