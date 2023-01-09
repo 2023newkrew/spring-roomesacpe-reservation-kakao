4dcc2db7549b3a12bc50691f331bbdd468d9bbd0
yooj929 <jang0929@naver.com>
2023-01-09 15:52:05 +0900

    feat: create reservation 서비스 구현

--- src/main/java/nextstep/service/ReservationServiceImpl.java
+++ src/main/java/nextstep/service/ReservationServiceImpl.java
@@ -1,14 +1,22 @@
 package nextstep.service;
 
 import nextstep.dto.ReservationRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReservationServiceImpl implements ReservationService {
 
+    private final ReservationService reservationService;
+
+    @Autowired
+    public ReservationServiceImpl(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
     @Override
     public Integer createReservation(ReservationRequestDTO reservationRequestDTO) {
 
-        return 1;
+        return reservationService.createReservation(reservationRequestDTO);
     }
 }
