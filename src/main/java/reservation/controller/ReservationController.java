7177da6d3e6de11c9bc8f5e278a7b01c5d5b72ee
kimtaehyun98 <kimtaehyun98@naver.com>
2023-01-09 17:19:02 +0900

    feat(reservation): 초기 세팅

---
+++ src/main/java/reservation/controller/ReservationController.java
@@ -0,0 +1,11 @@
+package reservation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+    @GetMapping("")
+    public 
+}
