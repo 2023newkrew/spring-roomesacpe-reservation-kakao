3908a8941d71a653e2fe01ac5ebb7f0996a20607
kimtaehyun98 <kimtaehyun98@naver.com>
2023-01-09 17:25:15 +0900

    feat(reservation): API controller 생성

--- src/main/java/reservation/controller/ReservationController.java
+++ src/main/java/reservation/controller/ReservationController.java
@@ -1,11 +1,24 @@
 package reservation.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reservation.domain.Reservation;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
-    @GetMapping("")
-    public 
+    @PostMapping("")
+    public ResponseEntity<Reservation> postReservation(@RequestBody Reservation reservation) {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{reservation_id}")
+    public ResponseEntity<Reservation> getReservation(@PathVariable Long reservation_id) {
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{reservation_id}")
+    public ResponseEntity<Reservation> deleteReservation(@PathVariable Long reservation_id) {
+        return ResponseEntity.ok().build();
+    }
 }
