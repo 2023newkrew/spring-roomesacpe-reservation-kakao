084be6dee1fec484b0195e35799707c4dc124678
jin.100 <jin.100@kakaocorp.com>
2023-01-09 16:57:41 +0900

    feat : 응답 형태 Response Entity로 변경

--- src/main/java/nextstep/controller/RoomEscapeController.java
+++ src/main/java/nextstep/controller/RoomEscapeController.java
@@ -4,6 +4,8 @@ import nextstep.Reservation;
 import nextstep.dto.CreateReservationRequest;
 import nextstep.service.RoomEscapeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,11 @@ public class RoomEscapeController {
     }
 
     @PostMapping("")
-    public Reservation createReservation(@RequestBody CreateReservationRequest createReservationRequest) {
-        return roomEscapeService.add(createReservationRequest);
+    public ResponseEntity<Reservation> createReservation(@RequestBody CreateReservationRequest createReservationRequest) {
+        Reservation reservation = roomEscapeService.add(createReservationRequest);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Location", "/reservations/" + reservation.getId())
+                .body(reservation);
     }
 }
