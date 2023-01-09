b1a90cbbf8adebb75394a06040ee65cd332603bb
엄태호 <qqsscc7342@gmail.com>
2023-01-09 15:52:54 +0900

    feat: 예약 생성 인터페이스 생성

---
+++ src/main/java/nextstep/main/java/nextstep/repository/ReservationRepository.java
@@ -0,0 +1,7 @@
+package nextstep.main.java.nextstep.repository;
+
+import nextstep.main.java.nextstep.domain.Reservation;
+
+public interface ReservationRepository {
+    public void save(Reservation reservation);
+}
