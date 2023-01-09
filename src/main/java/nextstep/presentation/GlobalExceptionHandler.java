bac17026d61f046af74596227476c26ff12e5d38
eddie.kk <eddie.kk@kakaocorp.com>
2023-01-09 18:14:33 +0900

    feat: 예외 처리 로직 구현 - controlleradvice를 활용하여 예외 처리

---
+++ src/main/java/nextstep/presentation/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package nextstep.presentation;
+
+import nextstep.dto.ErrorResponse;
+import nextstep.exception.DuplicateReservationException;
+import nextstep.exception.ReservationNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ReservationNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleReservationNotFoundException(ReservationNotFoundException e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(DuplicateReservationException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateReservationException(DuplicateReservationException e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+
+}
