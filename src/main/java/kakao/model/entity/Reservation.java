b8f90fd6d8ffd7f9b541a8a5bbd00874de15cc93
BaeRoNuI <xogh20321@gmail.com>
2023-01-10 17:12:54 +0900

    feat: seperate entity with dto

--- src/main/java/kakao/model/entity/Reservation.java
+++ src/main/java/kakao/model/entity/Reservation.java
@@ -1,2 +1,24 @@
-package kakao.model.entity;public class Reservation {
+package kakao.model.entity;
+
+import kakao.model.response.Theme;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+public class Reservation {
+    private final Long id;
+    private final LocalDate date;
+    private final LocalTime time;
+    private final String name;
+    private final Theme theme;
+
+    public Reservation(Long id, LocalDate date, LocalTime time, String name, Theme theme) {
+        this.id = id;
+        this.date = date;
+        this.time = time;
+        this.name = name;
+        this.theme = theme;
+    }
 }
