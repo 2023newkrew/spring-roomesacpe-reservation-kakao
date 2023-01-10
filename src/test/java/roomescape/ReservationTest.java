b8a8074e2545732d449d28aaf62d1fb2c57f83a3
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-10 15:55:09 +0900

    test: 테스트 코드 버그픽스

--- src/test/java/roomescape/ReservationTest.java
+++ src/test/java/roomescape/ReservationTest.java
@@ -1,28 +1,62 @@
 package roomescape;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
-import org.hamcrest.core.Is;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import roomescape.domain.ReservationRequest;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("Reservation Test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ReservationTest {
 
     @LocalServerPort
     int port;
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
     @BeforeAll
     void setUp() throws Exception {
         RestAssured.port = port;
+        this.mockMvc.perform(post("/reservations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(new ReservationRequest("2022-08-11", "13:00", "kayla")))
+        );
+        this.mockMvc.perform(post("/reservations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(new ReservationRequest("2022-08-12", "13:00", "jerrie")))
+        );
+    }
+
+    @DisplayName("예약 생성 테스트")
+    @Test
+    void createReservation() {
+        ReservationRequest reservationRequest = new ReservationRequest("2022-08-13", "13:00", "kayla");
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(reservationRequest)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value())
+                .header("Location", "/reservations/3");
     }
 
     @Test
@@ -34,13 +68,12 @@ public class ReservationTest {
         // When
         ResultActions overlapResultActions = mockMvc.perform(post("/reservations")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(this.objectMapper.writeValueAsString(overlapReservationRequest))
+                .content(objectMapper.writeValueAsString(overlapReservationRequest))
         );
 
         // Then
         overlapResultActions.andExpect(status().isUnprocessableEntity());
     }
-
 
     @DisplayName("예약 조회 테스트")
     @Test
@@ -51,7 +84,7 @@ public class ReservationTest {
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value())
                 .body("id", is(1))
-                .body("name", is(name));
+                .body("name", is("kayla"));
     }
 
     @DisplayName("예약 조회 거절 테스트")
