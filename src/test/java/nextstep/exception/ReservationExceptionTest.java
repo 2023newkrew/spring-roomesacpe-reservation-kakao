832bf872a056fa5ccbf856b6e6a2eba8dde35cd5
Neurumaru <ksj6659@gmail.com>
2023-01-10 13:35:07 +0900

    fix(room-escape): 오류 수정정

--- src/test/java/nextstep/exception/ReservationExceptionTest.java
+++ src/test/java/nextstep/exception/ReservationExceptionTest.java
@@ -3,16 +3,11 @@ package nextstep.exception;
 import io.restassured.RestAssured;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.stream.Stream;
 import nextstep.domain.Reservation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -21,15 +16,10 @@ import org.springframework.http.MediaType;
 
 @DisplayName("예외 처리")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ReservationExceptionTest {
 
     @LocalServerPort
     int port;
-
-    private static Stream<Arguments> getCreateReservationData() {
-        return Stream.of(Arguments.of("2022-08-11", "13:00", "name"));
-    }
 
     @BeforeEach
     void setUp() {
@@ -37,7 +27,6 @@ public class ReservationExceptionTest {
     }
 
     @DisplayName("content-type이 application/json이 아닌 경우 값을 받지 않는다.")
-    @Order(1)
     @ParameterizedTest
     @ValueSource(strings = {MediaType.TEXT_PLAIN_VALUE, MediaType.TEXT_HTML_VALUE,
             MediaType.TEXT_XML_VALUE, MediaType.APPLICATION_XML_VALUE})
@@ -48,15 +37,13 @@ public class ReservationExceptionTest {
     }
 
     @DisplayName("예약 생성) 예약 생성 시 날짜와 시간이 똑같은 예약이 이미 있는 경우 예약을 생성할 수 없다.")
-    @Order(2)
     @Test
     void failToCreateReservationAlreadyExist() {
-        Reservation reservation = new Reservation(null, LocalDate.parse("2022-08-11"),
+        Reservation reservation = new Reservation(null, LocalDate.parse("2022-08-10"),
                 LocalTime.parse("13:00:00"), "name", null);
 
         RestAssured.given().contentType(MediaType.APPLICATION_JSON_VALUE).body(reservation).when()
-                .post("/reservations").then().log().all().statusCode(HttpStatus.CREATED.value())
-                .header("Location", "/reservations/1");
+                .post("/reservations").then().log().all().statusCode(HttpStatus.CREATED.value());
 
         RestAssured.given().log().all().contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(reservation).when().post("/reservations").then().log().all()
@@ -64,7 +51,6 @@ public class ReservationExceptionTest {
     }
 
     @DisplayName("예약 생성) 값이 포함되지 않았을 경우 예약 생설 불가.")
-    @Order(3)
     @ParameterizedTest
     @ValueSource(strings = {"{\"name\":\"abc\",\"time\":\"13:00:00\"}",
             "{\"name\":\"abc\",\"date\":\"2022-08-12\"}",
--- src/test/java/nextstep/mapping/ReservationTest.java
+++ src/test/java/nextstep/mapping/ReservationTest.java
@@ -5,15 +5,10 @@ import static org.hamcrest.core.Is.is;
 import io.restassured.RestAssured;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.stream.Stream;
 import nextstep.domain.Reservation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.params.provider.Arguments;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -22,15 +17,10 @@ import org.springframework.http.MediaType;
 
 @DisplayName("웹 요청 / 응답 처리로 입출력 추가")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ReservationTest {
 
     @LocalServerPort
     int port;
-
-    private static Stream<Arguments> getCreateReservationData() {
-        return Stream.of(Arguments.of("2022-08-11", "13:00", "name"));
-    }
 
     @BeforeEach
     void setUp() {
@@ -38,7 +28,6 @@ public class ReservationTest {
     }
 
     @DisplayName("예약 하기")
-    @Order(1)
     @Test
     void createReservation() {
         Reservation reservation = new Reservation(null, LocalDate.parse("2022-08-11"),
@@ -46,11 +35,10 @@ public class ReservationTest {
 
         RestAssured.given().log().all().contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(reservation).when().post("/reservations").then().log().all()
-                .statusCode(HttpStatus.CREATED.value()).header("Location", "/reservations/1");
+                .statusCode(HttpStatus.CREATED.value());
     }
 
     @DisplayName("예약 조회")
-    @Order(2)
     @Test
     void showReservation() {
         Reservation reservation = new Reservation(null, LocalDate.parse("2022-08-12"),
@@ -66,7 +54,6 @@ public class ReservationTest {
     }
 
     @DisplayName("예약 취소")
-    @Order(3)
     @Test
     void deleteReservation() {
         Reservation reservation = new Reservation(null, LocalDate.parse("2022-08-13"),
