a89853069038e6e5577cf8f5e579276aaf188f7f
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-10 18:53:04 +0900

    test: 여러 개의 예약을 연속적으로 저장하는 테스트 코드 추가

--- src/test/java/ReservationMemoryRepositoryTest.java
+++ src/test/java/ReservationMemoryRepositoryTest.java
@@ -34,6 +34,32 @@ class ReservationMemoryRepositoryTest {
     }
 
 
+    @DisplayName("여러개의 예약을 연속적으로 저장한다.")
+    @Test
+    void save_multi() {
+        Reservation inputReservation1 = generateReservation(
+                null, "2023-01-01", "13:00", "kim", testTheme);
+        Reservation inputReservation2 = generateReservation(
+                null, "2023-01-02", "14:00", "lee", testTheme);
+        Reservation inputReservation3 = generateReservation(
+                null, "2023-01-03", "15:00", "park", testTheme);
+
+        Reservation savedReservation1 = repository.save(inputReservation1);
+        Reservation savedReservation2 = repository.save(inputReservation2);
+        Reservation savedReservation3 = repository.save(inputReservation3);
+
+        Reservation expected1 = generateReservation(
+                1L, "2023-01-01", "13:00", "kim", testTheme);
+        Reservation expected2 = generateReservation(
+                2L, "2023-01-02", "14:00", "lee", testTheme);
+        Reservation expected3 = generateReservation(
+                3L, "2023-01-03", "15:00", "park", testTheme);
+
+        Assertions.assertThat(savedReservation1).isEqualTo(expected1);
+        Assertions.assertThat(savedReservation2).isEqualTo(expected2);
+        Assertions.assertThat(savedReservation3).isEqualTo(expected3);
+    }
+
     private Reservation generateReservation(Long id, String date, String time, String name, Theme theme) {
         return new Reservation(
                 id,
