28a09352fddbf66234825245708e4b303b999aa3
jungin0507 <a01033046758@gmail.com>
2023-01-10 10:48:25 +0900

    feat: Reservation Dao 생성

---
+++ src/main/java/nextstep/console/dao/ReservationDao.java
@@ -0,0 +1,88 @@
+package nextstep.console.dao;
+
+import nextstep.domain.Reservation;
+import nextstep.domain.Theme;
+
+import java.sql.*;
+
+public class ReservationDao {
+    private Connection con;
+
+    public ReservationDao() {
+        try {
+            con = DriverManager.getConnection("jdbc:h2:~/test;AUTO_SERVER=true", "sa", "");
+            System.out.println("정상적으로 연결되었습니다.");
+        } catch (SQLException e) {
+            con = null;
+            System.err.println("연결 오류:" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void addReservation(Reservation reservation) {
+        try {
+            String sql = "INSERT INTO reservation (date, time, name, theme_name, theme_desc, theme_price) VALUES (?, ?, ?, ?, ?, ?);";
+            PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
+            ps.setDate(1, Date.valueOf(reservation.getDate()));
+            ps.setTime(2, Time.valueOf(reservation.getTime()));
+            ps.setString(3, reservation.getName());
+            ps.setString(4, reservation.getTheme()
+                    .getName());
+            ps.setString(5, reservation.getTheme()
+                    .getDesc());
+            ps.setInt(6, reservation.getTheme()
+                    .getPrice());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Reservation findById(Long id) {
+        try {
+            String sql = "SELECT * FROM reservation WHERE ID = ?;";
+            PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
+            ps.setLong(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Theme theme = Theme.builder()
+                        .name(rs.getString("theme_name"))
+                        .desc(rs.getString("theme_desc"))
+                        .price(rs.getInt("theme_price"))
+                        .build();
+                return Reservation.builder()
+                        .id(rs.getLong("id"))
+                        .date(rs.getDate("date")
+                                .toLocalDate())
+                        .time(rs.getTime("time")
+                                .toLocalTime())
+                        .name(rs.getString("name"))
+                        .theme(theme)
+                        .build();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public void deleteById(Long id) {
+        try {
+            String sql = "DELETE FROM reservation WHERE ID = ?;";
+            PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
+            ps.setLong(1, id);
+            ps.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void close() {
+        try {
+            if (con != null)
+                con.close();
+        } catch (SQLException e) {
+            System.err.println("con 오류:" + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/nextstep/web/repository/ReservationDao.java
@@ -0,0 +1,63 @@
+package nextstep.web.repository;
+
+import lombok.RequiredArgsConstructor;
+import nextstep.domain.Reservation;
+import nextstep.domain.Theme;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+@RequiredArgsConstructor
+public class ReservationDao implements ReservationRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    private final RowMapper<Reservation> actorRowMapper = (resultSet, rowNum) -> {
+        Theme theme = Theme.builder()
+                .name(resultSet.getString("theme_name"))
+                .desc(resultSet.getString("theme_desc"))
+                .price(resultSet.getInt("theme_price"))
+                .build();
+        return Reservation.builder()
+                .id(resultSet.getLong("id"))
+                .date(resultSet.getDate("date")
+                        .toLocalDate())
+                .time(resultSet.getTime("time")
+                        .toLocalTime())
+                .name(resultSet.getString("name"))
+                .theme(theme)
+                .build();
+    };
+    
+    public void save(Reservation reservation) {
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("reservation")
+                .usingGeneratedKeyColumns("id");
+        Theme theme = reservation.getTheme();
+        Map<String, Object> params = new HashMap<>();
+
+        params.put("date", reservation.getDate());
+        params.put("time", reservation.getTime());
+        params.put("name", reservation.getName());
+        params.put("theme_name", theme.getName());
+        params.put("theme_desc", theme.getDesc());
+        params.put("theme_price", theme.getPrice());
+
+        Number key = simpleJdbcInsert.executeAndReturnKey(params);
+        reservation.setId(key.longValue());
+    }
+
+    public Reservation findById(Long id) {
+        String sql = "SELECT * FROM reservation WHERE ID = ?;";
+        return jdbcTemplate.queryForObject(sql, actorRowMapper, id);
+    }
+
+    public void deleteById(Long id) {
+        String sql = "DELETE FROM reservation WHERE ID = ?;";
+        jdbcTemplate.update(sql, id);
+    }
+}
