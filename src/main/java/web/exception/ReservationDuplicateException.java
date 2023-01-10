b33b8bc20adf3b9e41b1fe26e128efa8fec12aec
정핸모 <gusah009@naver.com>
2023-01-10 18:25:04 +0900

    feat: ExceptionAdvice로 Exception handling

--- src/main/java/web/exception/ReservationDuplicateException.java
+++ src/main/java/web/exception/ReservationDuplicateException.java
@@ -1,9 +1,5 @@
 package web.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.CONFLICT)
 public class ReservationDuplicateException extends RuntimeException {
     public ReservationDuplicateException() {
         super("요청하신 시간대에 이미 예약이 있습니다.");
--- src/main/java/web/exception/ReservationNotFoundException.java
+++ src/main/java/web/exception/ReservationNotFoundException.java
@@ -1,9 +1,5 @@
 package web.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ReservationNotFoundException extends RuntimeException {
     public ReservationNotFoundException(long id) {
         super("ID가 " + id + "인 예약 정보를 찾을 수 없습니다.");
