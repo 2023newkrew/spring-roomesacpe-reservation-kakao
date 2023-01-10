cad0959754bed8fe24fed9478a771e56f6b94623
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-10 21:40:15 +0900

    feat: 예약 조회 요청이 성공하면 예약 객체를 바디에 담아서 응답

--- src/main/java/nextstep/controller/ReservationController.java
+++ src/main/java/nextstep/controller/ReservationController.java
@@ -4,9 +4,7 @@ import nextstep.domain.Reservation;
 import nextstep.service.ReservationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -24,5 +22,11 @@ public class ReservationController {
     public ResponseEntity<Object> reserve(@RequestBody Reservation reservation){
         Reservation confirmedReservation = reservationService.reserve(reservation);
         return ResponseEntity.created(URI.create("/reservations/" + confirmedReservation.getId())).build();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Reservation findReservation(@PathVariable long id){
+        return reservationService.findReservation(id);
     }
 }
