372c4fa82d6b403c3ff5afde23211367d8df5656
gh1224 <rlarkgus0@naver.com>
2023-01-10 15:47:09 +0900

    feat: 웹 애플리케이션에서 예약 생성 dao 구현

---
+++ src/main/java/nextstep/web/ReservationUpdatingDAO.java
@@ -0,0 +1,38 @@
+package nextstep.web;
+
+import nextstep.Reservation;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.Time;
+
+@Repository
+public class ReservationUpdatingDAO {
+    private JdbcTemplate jdbcTemplate;
+
+    public ReservationUpdatingDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Long insertWithKeyHolder(Reservation reservation) {
+        String sql = "INSERT INTO reservation (date, time, name, theme_name, theme_desc, theme_price) VALUES (?, ?, ?, ?, ?, ?);";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setDate(1, Date.valueOf(reservation.getDate()));
+            ps.setTime(2, Time.valueOf(reservation.getTime()));
+            ps.setString(3, reservation.getName());
+            ps.setString(4, reservation.getTheme().getName());
+            ps.setString(5, reservation.getTheme().getDesc());
+            ps.setInt(6, reservation.getTheme().getPrice());
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
+    }
+}
---
+++ src/test/java/nextstep/web/ReservationUpdatingDAOTest.java
@@ -0,0 +1,58 @@
+package nextstep.web;
+
+import nextstep.Reservation;
+import nextstep.Theme;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class ReservationUpdatingDAOTest {
+    @Autowired
+    private ReservationUpdatingDAO reservationUpdatingDAO;
+    @Autowired
+    private ReservationQueryingDAO reservationQueryingDAO;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        List<Object[]> reservations = List.of(
+                new Object[]{Date.valueOf("2022-08-11"), Time.valueOf("13:00:00"), "name", "워너고홈", "병맛 어드벤처 회사 코믹물", 29_000},
+                new Object[]{Date.valueOf("2022-08-11"), Time.valueOf("14:00:00"), "name2", "워너고홈", "병맛 어드벤처 회사 코믹물", 29_000}
+        );
+
+        jdbcTemplate.batchUpdate("INSERT INTO reservation (date, time, name, theme_name, theme_desc, theme_price) VALUES (?, ?, ?, ?, ?, ?);", reservations);
+    }
+
+    @Test
+    void insertWithKeyHolder() {
+        Reservation reservation = new Reservation(
+                LocalDate.of(2022, 8, 11),
+                LocalTime.of(13, 0),
+                "name3",
+                new Theme(
+                        "워너고홈",
+                        "병맛 어드벤처 회사 코믹물",
+                        29000
+                )
+        );
+        Long id = reservationUpdatingDAO.insertWithKeyHolder(reservation);
+
+        assertThat(id).isNotNull();
+
+        Reservation reservationById = reservationQueryingDAO.findReservationById(id);
+        assertThat(reservationById).isNotNull();
+        assertThat(reservationById.getName()).isEqualTo("name3");
+    }
+}
