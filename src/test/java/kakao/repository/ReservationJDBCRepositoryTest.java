b18355c5fc2d66210e95184aa3738ff4f6b29f5b
baker.gus <baker.gus@kakaocorp.com>
2023-01-10 14:49:51 +0900

    refactor: minor change

--- src/test/java/kakao/repository/ReservationJDBCRepositoryTest.java
+++ src/test/java/kakao/repository/ReservationJDBCRepositoryTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -29,7 +28,6 @@ public class ReservationJDBCRepositoryTest {
             ThemeRepository.theme
     );
 
-    @Transactional
     @BeforeEach
     void setUp() {
         jdbcTemplate.execute("TRUNCATE TABLE reservation");
