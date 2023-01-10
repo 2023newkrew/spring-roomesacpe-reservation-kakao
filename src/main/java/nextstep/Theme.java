fc9d7e65b677cd9b94933c8e3c7645184b037a6b
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-10 18:33:00 +0900

    feat: Theme에 equals(), hashCode() 구현

--- src/main/java/nextstep/Theme.java
+++ src/main/java/nextstep/Theme.java
@@ -1,5 +1,7 @@
 package nextstep;
 
+import java.util.Objects;
+
 public class Theme {
     private String name;
     private String desc;
@@ -21,5 +23,18 @@ public class Theme {
 
     public Integer getPrice() {
         return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Theme theme = (Theme) o;
+        return Objects.equals(name, theme.name) && Objects.equals(desc, theme.desc) && Objects.equals(price, theme.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, desc, price);
     }
 }
