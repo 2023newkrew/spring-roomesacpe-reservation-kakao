7dc02778cb2d4d80e3d377d79d7ba18b280a289c
park0503 <hooo0503@gmail.com>
2023-01-10 14:30:17 +0900

    feat: 존재하지 않는 API 호출 예외처리

--- src/main/java/kakao/error/ErrorCode.java
+++ src/main/java/kakao/error/ErrorCode.java
@@ -1,6 +1,10 @@
 package kakao.error;
 
 public enum ErrorCode {
+    // 공통 에러
+    API_NOT_FOUND(404, "잘못된 API 요청입니다."),
+
+    // 예약 에러
     DUPLICATE_RESERVATION(400, "해당 시간에 예약이 존재합니다."),
     RESERVATION_NOT_FOUND(400, "해당 ID의 예약이 존재하지 않습니다.");
 
--- src/main/java/kakao/error/GlobalExceptionHandler.java
+++ src/main/java/kakao/error/GlobalExceptionHandler.java
@@ -5,19 +5,47 @@ import kakao.error.exception.RecordNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.server.MethodNotAllowedException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    //Custom
-    @ExceptionHandler(DuplicatedReservationException.class)
-    public ResponseEntity<ErrorResponse> handleDuplicatedReservationException(DuplicatedReservationException e) {
-        ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
-        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+
+    // 담당 컨트롤러조차 발견하지 못했을 경우 발생
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoHandlerFoundException(NoHandlerFoundException e) {
+        return handlerException(ErrorCode.API_NOT_FOUND);
     }
 
+    // 담당 컨트롤러는 존재하나 해당하는 컨트롤러 메소드가 없을 시 발생
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        return handlerException(ErrorCode.API_NOT_FOUND);
+    }
+
+    // url에 해당하는 컨트롤러 메소드는 존재하나 Http 메소드가 옳바르지 않을 시 발생
+    @ExceptionHandler(MethodNotAllowedException.class)
+    public ResponseEntity<ErrorResponse> handleMethodNotAllowedException(MethodNotAllowedException e) {
+        return handlerException(ErrorCode.API_NOT_FOUND);
+    }
+
+    //Custom
+
+    // 이미 예약이 되어 있는 시간대에 예약을 시도할 경우 발생
+    @ExceptionHandler(DuplicatedReservationException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicatedReservationException(DuplicatedReservationException e) {
+        return handlerException(e.getErrorCode());
+    }
+
+    // 해당 id의 row가 존재하지 않을 시 발생
     @ExceptionHandler(RecordNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleRecordNotFoundException(RecordNotFoundException e) {
-        ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
+        return handlerException(e.getErrorCode());
+    }
+
+    private ResponseEntity<ErrorResponse> handlerException(ErrorCode errorCode) {
+        ErrorResponse errorResponse = ErrorResponse.of(errorCode);
         return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
     }
 }
