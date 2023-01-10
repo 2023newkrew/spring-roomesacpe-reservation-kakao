5bf50bba942af243da0ae7b0cf2bab5e8c1f9217
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-11 00:49:11 +0900

    feat: 예약 JDBC 리포지토리의 저장 기능 구현

--- src/main/java/nextstep/repository/ReservationJdbcRepository.java
+++ src/main/java/nextstep/repository/ReservationJdbcRepository.java
@@ -2,13 +2,48 @@ package nextstep.repository;
 
 import nextstep.domain.Reservation;
 
+import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 
 public class ReservationJdbcRepository implements ReservationRepository{
+
+    private static final String DB_URL = "jdbc:h2:tcp://localhost/~/Projects/h2/db/roomescape;AUTO_SERVER=true";
+    private static final String DB_USER = "sa";
+    private static final String DB_PASSWORD = "";
+
     @Override
     public Reservation save(Reservation reservation) {
-        return null;
+        String sql = "INSERT INTO reservation (date, time, name, theme_name, theme_desc, theme_price) VALUES (?, ?, ?, ?, ?, ?);";
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet resultSet = null;
+
+        try {
+            conn = getConnection();
+            ps = conn.prepareStatement(sql, new String[]{"id"});
+
+            ps.setDate(1, Date.valueOf(reservation.getDate()));
+            ps.setTime(2, Time.valueOf(reservation.getTime()));
+            ps.setString(3, reservation.getName());
+            ps.setString(4, reservation.getTheme().getName());
+            ps.setString(5, reservation.getTheme().getDesc());
+            ps.setInt(6, reservation.getTheme().getPrice());
+
+            ps.executeUpdate();
+
+            resultSet = ps.getGeneratedKeys();
+            if (!resultSet.next()) {
+                throw new SQLException();
+            }
+
+            reservation.setId(resultSet.getLong("id"));
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, ps, resultSet);
+        }
+        return reservation;
     }
 
     @Override
@@ -24,5 +59,33 @@ public class ReservationJdbcRepository implements ReservationRepository{
     @Override
     public boolean delete(Long id) {
         return false;
+    }
+
+    private Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+    }
+
+    private void close(Connection conn, PreparedStatement ps, ResultSet resultSet) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+        if (ps != null) {
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        }
     }
 }
