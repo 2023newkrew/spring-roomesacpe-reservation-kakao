3bf0a943dc4cfb432dd09b6808185fc049ca2e84
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-11 01:16:47 +0900

    feat: ReservationJdbcTemplateRepository 추가

---
+++ src/main/java/nextstep/repository/ReservationJdbcTemplateRepository.java
@@ -0,0 +1,30 @@
+package nextstep.repository;
+
+import nextstep.domain.Reservation;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ReservationJdbcTemplateRepository implements ReservationRepository {
+    @Override
+    public Reservation save(Reservation reservation) {
+        return null;
+    }
+
+    @Override
+    public Optional<Reservation> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Reservation> findAll() {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
