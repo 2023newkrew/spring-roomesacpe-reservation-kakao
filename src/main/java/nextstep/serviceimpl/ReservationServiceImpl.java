b652517cacd864989ee6db18800ae3c890bb28ba
gkwodn1044 <gkwodn1044@gmail.com>
2023-01-10 18:29:40 +0900

    feat : 예외 처리

--- src/main/java/nextstep/serviceimpl/ReservationServiceImpl.java
+++ src/main/java/nextstep/serviceimpl/ReservationServiceImpl.java
@@ -24,16 +24,26 @@ public class ReservationServiceImpl implements ReservationService {
 
     @Override
     public Reservation create(ReservationRequest request) {
-        Reservation reservation = new Reservation(
-                ++reservationIdIndex,
-                LocalDate.parse(request.getDate()),
-                LocalTime.parse(request.getTime() + ":00"),
-                request.getName(),
-                theme
-        );
-
+        LocalDate date = LocalDate.parse(request.getDate());
+        LocalTime time = LocalTime.parse(request.getTime() + ":00");
+        String name = request.getName();
+        validateDateTime(date, time);
+        Reservation reservation = new Reservation(++reservationIdIndex, date, time, name, theme);
         reservations.put(reservationIdIndex, reservation);
+
         return reservation;
+    }
+
+    private void validateDateTime(LocalDate date, LocalTime time) {
+        if (isDuplicated(date, time)) {
+            throw new IllegalArgumentException("해당 시간에 이미 예약이 존재합니다.");
+        }
+    }
+
+    private boolean isDuplicated(LocalDate date, LocalTime time) {
+        return reservations.values()
+                .stream()
+                .anyMatch(r -> r.getDate().equals(date) && r.getTime().equals(time));
     }
 
     @Override
