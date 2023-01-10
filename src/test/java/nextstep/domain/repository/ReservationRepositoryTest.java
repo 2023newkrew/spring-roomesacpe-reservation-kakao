6084f0923fba1883f2dd7360f774b9ddfb4d5750
eddie.kk <eddie.kk@kakaocorp.com>
2023-01-10 23:20:59 +0900

    refactor: repository 테스트 개선 - DatabaseExecutor 구현을 통해 테이블 생성 및 테스트 격리 - 모든 테스트 시작 전에 테스트 테이블을 생성하도록 설정

---
+++ src/test/java/nextstep/domain/repository/ReservationRepositoryTest.java
@@ -0,0 +1,102 @@
+package nextstep.domain.repository;
+
+import nextstep.common.DatabaseExecutor;
+import nextstep.domain.Reservation;
+import nextstep.domain.Theme;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Optional;
+
+import static nextstep.domain.repository.QuerySetting.Reservation.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest(includeFilters = {
+        @Filter(type = FilterType.ANNOTATION, classes = Repository.class),
+        @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = { DatabaseExecutor.class })
+    }
+)
+public class ReservationRepositoryTest {
+
+    private static final Theme DEFAULT_THEME = new Theme("워너고홈", "병맛 어드벤처 회사 코믹물", 29_000);
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @BeforeAll
+    static void createTable(@Autowired DatabaseExecutor databaseExecutor) {
+        databaseExecutor.createTable(TABLE_NAME);
+    }
+
+    @Test
+    void 예약을_생성한다() {
+        // given
+        Reservation reservation = new Reservation(LocalDate.parse("2023-01-09"), LocalTime.parse("13:00"), "eddie-davi", DEFAULT_THEME);
+
+        // when
+        Reservation savedReservation = reservationRepository.save(reservation);
+
+        // then
+        assertThat(savedReservation.getId()).isNotNull();
+        assertThat(savedReservation).usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(reservation);
+    }
+
+    @Test
+    void 예약을_id로_조회한다() {
+        // given
+        Reservation reservation = new Reservation(LocalDate.parse("2023-01-09"), LocalTime.parse("13:00"), "eddie-davi", DEFAULT_THEME);
+        Reservation savedReservation = reservationRepository.save(reservation);
+        Long id = savedReservation.getId();
+
+        // when
+        Reservation findById = reservationRepository.findById(id)
+                .orElseThrow();
+
+        // then
+        assertThat(savedReservation).usingRecursiveComparison()
+                .isEqualTo(findById);
+    }
+
+    @Test
+    void id에_해당하는_예약이_없을_경우_아무것도_반환되지_않는다() {
+        // given
+        Reservation reservation = new Reservation(LocalDate.parse("2023-01-09"), LocalTime.parse("13:00"), "eddie-davi", DEFAULT_THEME);
+        Reservation savedReservation = reservationRepository.save(reservation);
+        Long invalidId = savedReservation.getId() + 1000;
+
+        // when
+        Optional<Reservation> findById = reservationRepository.findById(invalidId);
+
+        // then
+        assertThat(findById).isEqualTo(Optional.empty());
+    }
+
+    @Test
+    void 날짜와_시간이_같은_예약을_조회한다() {
+        // given
+        Reservation reservation = new Reservation(LocalDate.parse("2023-01-09"), LocalTime.parse("13:00"), "eddie-davi", DEFAULT_THEME);
+        reservationRepository.save(reservation);
+
+        // when, then
+        assertThat(reservationRepository.existsByDateAndTime(reservation.getDate(), reservation.getTime())).isTrue();
+    }
+
+    @Test
+    void id에_해당하는_예약을_삭제한다() {
+        // given
+        Reservation reservation = new Reservation(LocalDate.parse("2023-01-09"), LocalTime.parse("13:00"), "eddie-davi", DEFAULT_THEME);
+        Long reservationId = reservationRepository.save(reservation).getId();
+
+        // when, then
+        assertThat(reservationRepository.deleteById(reservationId)).isTrue();
+    }
+}
