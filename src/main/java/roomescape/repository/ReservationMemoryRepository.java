0115a93a5ad98492aaf7d3bb61bf90baaf1abf70
pyun0825 <pyun0825@naver.com>
2023-01-10 14:32:25 +0900

    feat: Reservation Repository 추가

---
+++ src/main/java/roomescape/repository/ReservationMemoryRepository.java
@@ -0,0 +1,40 @@
+package roomescape.repository;
+
+import org.springframework.stereotype.Repository;
+import roomescape.model.Reservation;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class ReservationMemoryRepository implements ReservationRepository{
+    private static Map<Long, Reservation> reservations = new HashMap<>();
+    private static Long reservationIdIndex = 0L;
+
+    @Override
+    public long save(Reservation reservation) {
+        reservation.setId(reservationIdIndex);
+        reservations.put(reservationIdIndex, reservation);
+        return reservationIdIndex++;
+    }
+
+    @Override
+    public Optional<Reservation> findOneById(long reservationId) {
+        return Optional.ofNullable(reservations.get(reservationId));
+    }
+
+    @Override
+    public void delete(long reservationId) {
+        reservations.remove(reservationId);
+    }
+
+    @Override
+    public Boolean hasOneByDateAndTime(LocalDate date, LocalTime time) {
+        return reservations.values()
+                .stream()
+                .anyMatch(reservation -> reservation.getDate().equals(date) && reservation.getTime().equals(time));
+    }
+}
---
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -0,0 +1,14 @@
+package roomescape.repository;
+
+import roomescape.model.Reservation;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Optional;
+
+public interface ReservationRepository {
+    long save(Reservation reservation);
+    Optional<Reservation> findOneById(long reservationId);
+    void delete(long reservationId);
+    Boolean hasOneByDateAndTime(LocalDate date, LocalTime time);
+}
