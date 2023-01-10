baf71ef8e572cd4a654ed606495091102abbca8e
gh1224 <rlarkgus0@naver.com>
2023-01-10 15:59:03 +0900

    feat: 웹 컨트롤러에 데이터베이스 연동

--- src/main/java/nextstep/controller/ReservationController.java
+++ src/main/java/nextstep/controller/ReservationController.java
@@ -3,55 +3,50 @@ package nextstep.controller;
 import nextstep.Reservation;
 import nextstep.ReservationInfo;
 import nextstep.Theme;
-import nextstep.exceptions.CustomException;
+import nextstep.web.ReservationQueryingDAO;
+import nextstep.web.ReservationUpdatingDAO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
-    private final AtomicLong counter = new AtomicLong();
-    private final List<Reservation> reservations = new ArrayList<>();
+    private final ReservationQueryingDAO reservationQueryingDAO;
+    private final ReservationUpdatingDAO reservationUpdatingDAO;
     private final Theme theme = new Theme("워너고홈", "병맛 어드벤처 회사 코믹물", 29_000);
+
+    public ReservationController(ReservationQueryingDAO reservationQueryingDAO, ReservationUpdatingDAO reservationUpdatingDAO) {
+        this.reservationQueryingDAO = reservationQueryingDAO;
+        this.reservationUpdatingDAO = reservationUpdatingDAO;
+    }
 
     @PostMapping("")
     public ResponseEntity createReservation(@RequestBody Reservation reservation) {
-        long id = counter.incrementAndGet();
-        Reservation newReservation = new Reservation(id, reservation.getDate(), reservation.getTime(), reservation.getName(), theme);
+        Reservation newReservation = new Reservation(reservation.getDate(), reservation.getTime(), reservation.getName(), theme);
 
-        boolean existsReservation = reservations.stream()
-                .anyMatch(r -> r.getDate().equals(reservation.getDate())
-                        && r.getTime().equals(reservation.getTime()));
-        if (existsReservation) {
-            throw new CustomException();
-        }
+//        boolean existsReservation = reservations.stream()
+//                .anyMatch(r -> r.getDate().equals(reservation.getDate())
+//                        && r.getTime().equals(reservation.getTime()));
+//        if (existsReservation) {
+//            throw new CustomException();
+//        }
 
-        reservations.add(newReservation);
+        Long id = reservationUpdatingDAO.insertWithKeyHolder(newReservation);
         return ResponseEntity.created(URI.create("/reservations/" + id)).build();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity showReservation(@PathVariable Long id) {
-        Reservation reservation = reservations.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .orElseThrow();
+        Reservation reservation = reservationQueryingDAO.findReservationById(id);
         ReservationInfo reservationInfo = new ReservationInfo(reservation);
         return ResponseEntity.ok().body(reservationInfo);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity deleteReservation(@PathVariable Long id) {
-        Reservation reservation = reservations.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .orElseThrow();
-        reservations.remove(reservation);
+        reservationUpdatingDAO.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
