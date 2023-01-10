c2ca3cd655f1ca330c738cff2f6addd707d21c7c
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-10 13:26:49 +0900

    feat: reservation request 클래스 생성 및 reservation 생성자 추가

--- src/main/java/nextstep/Reservation.java
+++ src/main/java/nextstep/Reservation.java
@@ -2,6 +2,7 @@ package nextstep;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Reservation {
     private Long id;
@@ -18,8 +19,23 @@ public class Reservation {
         this.theme = theme;
     }
 
+    public Reservation(ReservationRequest reservationRequest) {
+        this.date = LocalDate.parse(reservationRequest.getDate(), DateTimeFormatter.ISO_DATE);
+        this.time = LocalTime.parse(reservationRequest.getTime());
+        this.name = reservationRequest.getName();
+    }
+
+    public Reservation(ReservationRequest reservationRequest, Long id) {
+        this(reservationRequest);
+        this.id = id;
+    }
+
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public LocalDate getDate() {
@@ -36,5 +52,9 @@ public class Reservation {
 
     public Theme getTheme() {
         return theme;
+    }
+
+    public boolean overlap(Reservation other) {
+        return this.date.equals(other.date) && this.time.equals(other.time);
     }
 }
---
+++ src/main/java/roomescape/domain/ReservationRequest.java
@@ -0,0 +1,28 @@
+package roomescape.domain;
+
+public class ReservationRequest {
+
+    private String date;
+    private String time;
+    private String name;
+
+    public ReservationRequest(String date, String time, String name) {
+        this.date = date;
+        this.time = time;
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
