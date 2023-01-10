e29dab76da2fd386c6bd21bf8b64ea22885b69cf
Ethan <rlfalsgh95@naver.com>
2023-01-10 14:58:18 +0900

    feat : ReservationDto validation 적용

--- src/main/java/nextstep/dto/ReservationDto.java
+++ src/main/java/nextstep/dto/ReservationDto.java
@@ -3,6 +3,8 @@ package nextstep.dto;
 import lombok.*;
 import nextstep.entity.Reservation;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -12,9 +14,18 @@ import java.time.format.DateTimeFormatter;
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class ReservationDto {
+    @NonNull
+    @Pattern(regexp = "{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])")
     private String date;
+
+    @NonNull
+    @Pattern(regexp = "(0[1-9]|1[0-9]|2[0-4]):(0[1-9]|[1-5][0-9])")
     private String time;
+
+    @NotBlank
     private String name;
+
+    @Setter
     private Long themeId;
 
     public LocalDate getLocalDate() {
