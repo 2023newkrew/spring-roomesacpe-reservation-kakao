2e9d28426258d285f0ec56aaf45966c95d09086b
eddie.kk <eddie.kk@kakaocorp.com>
2023-01-10 13:40:21 +0900

    feat: 콘솔 애플리케이션 예약 조회 기능

--- src/main/java/nextstep/ConsoleRoomEscapeApplication.java
+++ src/main/java/nextstep/ConsoleRoomEscapeApplication.java
@@ -4,12 +4,10 @@ import nextstep.domain.Reservation;
 import nextstep.domain.Theme;
 import nextstep.domain.repository.ConsoleReservationRepository;
 import nextstep.domain.repository.ReservationRepository;
+import nextstep.exception.ReservationNotFoundException;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class ConsoleRoomEscapeApplication {
@@ -56,25 +54,23 @@ public class ConsoleRoomEscapeApplication {
                 System.out.println("예약자 이름: " + savedReservation.getName());
             }
 
-//            if (input.startsWith(FIND)) {
-//                String params = input.split(" ")[1];
-//
-//                Long id = Long.parseLong(params.split(",")[0]);
-//
-//                Reservation reservation = reservations.stream()
-//                        .filter(it -> Objects.equals(it.getId(), id))
-//                        .findFirst()
-//                        .orElseThrow(RuntimeException::new);
-//
-//                System.out.println("예약 번호: " + reservation.getId());
-//                System.out.println("예약 날짜: " + reservation.getDate());
-//                System.out.println("예약 시간: " + reservation.getTime());
-//                System.out.println("예약자 이름: " + reservation.getName());
-//                System.out.println("예약 테마 이름: " + reservation.getTheme().getName());
-//                System.out.println("예약 테마 설명: " + reservation.getTheme().getDesc());
-//                System.out.println("예약 테마 가격: " + reservation.getTheme().getPrice());
-//            }
-//
+            if (input.startsWith(FIND)) {
+                String params = input.split(" ")[1];
+
+                Long id = Long.parseLong(params.split(",")[0]);
+
+                Reservation reservation = reservationRepository.findById(id)
+                        .orElseThrow(ReservationNotFoundException::new);
+
+                System.out.println("예약 번호: " + reservation.getId());
+                System.out.println("예약 날짜: " + reservation.getDate());
+                System.out.println("예약 시간: " + reservation.getTime());
+                System.out.println("예약자 이름: " + reservation.getName());
+                System.out.println("예약 테마 이름: " + reservation.getTheme().getName());
+                System.out.println("예약 테마 설명: " + reservation.getTheme().getDesc());
+                System.out.println("예약 테마 가격: " + reservation.getTheme().getPrice());
+            }
+
 //            if (input.startsWith(DELETE)) {
 //                String params = input.split(" ")[1];
 //
