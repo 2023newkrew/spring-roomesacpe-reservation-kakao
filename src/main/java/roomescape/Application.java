f634774a53df0452bbe2e2be7b521a33015eb0cd
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-10 13:25:38 +0900

    feat: application 작성

---
+++ src/main/java/roomescape/Application.java
@@ -0,0 +1,11 @@
+package roomescape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
