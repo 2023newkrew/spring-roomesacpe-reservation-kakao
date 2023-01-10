074a42faf9c5122f0de1c2125e8a886023f2b247
정핸모 <gusah009@naver.com>
2023-01-10 18:22:02 +0900

    feat: ReservationDuplicateException에 에러 메시지 추가

--- src/main/java/web/exception/ReservationDuplicateException.java
+++ src/main/java/web/exception/ReservationDuplicateException.java
@@ -6,5 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.CONFLICT)
 public class ReservationDuplicateException extends RuntimeException {
     public ReservationDuplicateException() {
+        super("요청하신 시간대에 이미 예약이 있습니다.");
     }
 }
