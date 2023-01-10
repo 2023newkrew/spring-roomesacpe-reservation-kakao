01240e2d6dc96b88013ed479aaf240d444b83723
pyun0825 <pyun0825@naver.com>
2023-01-10 14:34:16 +0900

    feat: Controller에 service 연결

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,19 +1,33 @@
 package roomescape.controller;
 
+import nextstep.Reservation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import roomescape.dto.ReservationRequestDto;
+import roomescape.dto.ReservationResponseDto;
+import roomescape.service.ReservationService;
 
 import java.net.URI;
 
 @Controller
 public class ReservationController {
 
+    private ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping("/reservations/{id}")
+    public ResponseEntity<ReservationResponseDto> findReservation(@PathVariable("id") Long reservationId) {
+        ReservationResponseDto reservationResponseDto = reservationService.findReservation(reservationId);
+        return ResponseEntity.ok(reservationResponseDto);
+    }
+
     @PostMapping("/reservations")
     public ResponseEntity createReservation(@RequestBody ReservationRequestDto dto) {
-        Long id = 1L;
-        return ResponseEntity.created(URI.create("/reservations/" + id)).build();
+        Long reservationId = reservationService.createReservation(dto);
+        return ResponseEntity.created(URI.create("/reservations/" + reservationId)).build();
     }
 }
