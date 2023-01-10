3218d41b765be0670b8e2d2e1990635b71c1d98b
jay.den <jay.den@kakaocorp.com>
2023-01-10 15:52:13 +0900

    feat : 콘솔에 db 연동
    - 기존 콘솔 예약 프로그램에 db 연동

---
+++ src/main/java/nextstep/ReservationConsole.java
@@ -0,0 +1,86 @@
+package nextstep;
+
+import nextstep.repository.ReservationDao;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class ReservationConsole {
+    private static final String ADD = "add";
+    private static final String FIND = "find";
+    private static final String DELETE = "delete";
+    private static final String QUIT = "quit";
+
+    private static final ReservationDao reservationDao = new ReservationDao();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Theme theme = new Theme("워너고홈", "병맛 어드벤처 회사 코믹물", 29_000);
+
+        while (true) {
+            System.out.println();
+            System.out.println("### 명령어를 입력하세요. ###");
+            System.out.println("- 예약하기: add {date},{time},{name} ex) add 2022-08-11,13:00,류성현");
+            System.out.println("- 예약조회: find {id} ex) find 1");
+            System.out.println("- 예약취소: delete {id} ex) delete 1");
+            System.out.println("- 종료: quit");
+
+            String input = scanner.nextLine();
+            if (input.startsWith(ADD)) {
+                String params = input.split(" ")[1];
+
+                String date = params.split(",")[0];
+                String time = params.split(",")[1];
+                String name = params.split(",")[2];
+
+                Reservation reservation = new Reservation(
+                        LocalDate.parse(date),
+                        LocalTime.parse(time + ":00"),
+                        name,
+                        theme
+                );
+
+                Long id = reservationDao.addReservationForConsole(reservation);
+                if (id != null) {
+                    System.out.println("예약이 등록되었습니다.");
+                    System.out.println("예약 번호: " + id);
+                    System.out.println("예약 날짜: " + reservation.getDate());
+                    System.out.println("예약 시간: " + reservation.getTime());
+                    System.out.println("예약자 이름: " + reservation.getName());
+                }
+            }
+
+            if (input.startsWith(FIND)) {
+                String params = input.split(" ")[1];
+
+                Long id = Long.parseLong(params.split(",")[0]);
+
+                Reservation reservation = reservationDao.findReservationForConsole(id);
+                if (reservation != null) {
+                    System.out.println("예약 번호: " + reservation.getId());
+                    System.out.println("예약 날짜: " + reservation.getDate());
+                    System.out.println("예약 시간: " + reservation.getTime());
+                    System.out.println("예약자 이름: " + reservation.getName());
+                    System.out.println("예약 테마 이름: " + reservation.getTheme().getName());
+                    System.out.println("예약 테마 설명: " + reservation.getTheme().getDesc());
+                    System.out.println("예약 테마 가격: " + reservation.getTheme().getPrice());
+                }
+            }
+
+            if (input.startsWith(DELETE)) {
+                String params = input.split(" ")[1];
+
+                Long id = Long.parseLong(params.split(",")[0]);
+
+                reservationDao.deleteReservationForConsole(id);
+                System.out.println("예약이 취소되었습니다.");
+            }
+
+            if (input.equals(QUIT)) {
+                break;
+            }
+        }
+    }
+}
--- src/main/java/nextstep/repository/ReservationDao.java
+++ src/main/java/nextstep/repository/ReservationDao.java
@@ -1,8 +1,11 @@
 package nextstep.repository;
 
 import nextstep.Reservation;
+import nextstep.Theme;
+import nextstep.exceptions.exception.DuplicatedDateAndTimeException;
 import org.springframework.stereotype.Repository;
 
+import java.sql.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -27,16 +30,151 @@ public class ReservationDao {
     }
 
     public void delete(Long id) {
-        reservations.remove(id);
+        reservations.remove(id.intValue());
     }
 
     public Reservation findByDateAndTime(LocalDate date, LocalTime time) {
         List<Reservation> result = reservations.stream().filter(reservation -> {
-            return reservation.getDate().equals(date)  && reservation.getTime().equals(time);
+            return reservation.getDate().equals(date) && reservation.getTime().equals(time);
         }).collect(Collectors.toList());
         if (result.size() != 0) {
             return result.get(0);
         }
         return null;
+    }
+
+    public Long addReservationForConsole(Reservation reservation) {
+        Connection con = null;
+
+        // 드라이버 연결
+        try {
+            con = DriverManager.getConnection("jdbc:h2:~/test;AUTO_SERVER=true", "sa", "");
+            System.out.println("정상적으로 연결되었습니다.");
+        } catch (SQLException e) {
+            System.err.println("연결 오류:" + e.getMessage());
+            e.printStackTrace();
+        }
+        Long id = null;
+        try {
+            String sql = "SELECT * FROM reservation WHERE date = ? and time = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setDate(1, Date.valueOf(reservation.getDate()));
+            ps.setTime(2, Time.valueOf(reservation.getTime()));
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                throw new DuplicatedDateAndTimeException();
+            }
+
+            sql = "INSERT INTO reservation (date, time, name, theme_name, theme_desc, theme_price) VALUES (?, ?, ?, ?, ?, ?);";
+            ps = con.prepareStatement(sql, new String[]{"id"});
+            ps.setDate(1, Date.valueOf(reservation.getDate()));
+            ps.setTime(2, Time.valueOf(reservation.getTime()));
+            ps.setString(3, reservation.getName());
+            ps.setString(4, reservation.getTheme().getName());
+            ps.setString(5, reservation.getTheme().getDesc());
+            ps.setInt(6, reservation.getTheme().getPrice());
+            ps.executeUpdate();
+
+            rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                id = rs.getLong(1);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DuplicatedDateAndTimeException e) {
+            System.out.println("해당 날짜와 시간은 이미 예약되었습니다.");
+        }
+
+        try {
+            if (con != null)
+                con.close();
+        } catch (SQLException e) {
+            System.err.println("con 오류:" + e.getMessage());
+        }
+
+        return id;
+    }
+
+    public Reservation findReservationForConsole(Long id) {
+        Connection con = null;
+        ResultSet rs;
+        Reservation reservation;
+
+        // 드라이버 연결
+        try {
+            con = DriverManager.getConnection("jdbc:h2:~/test;AUTO_SERVER=true", "sa", "");
+            System.out.println("정상적으로 연결되었습니다.");
+        } catch (SQLException e) {
+            System.err.println("연결 오류:" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            String sql = "SELECT * FROM reservation WHERE id = ?;";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setLong(1, id);
+            rs = ps.executeQuery();
+            reservation = getResultFromResultSet(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            if (con != null)
+                con.close();
+        } catch (SQLException e) {
+            System.err.println("con 오류:" + e.getMessage());
+        }
+
+        return reservation;
+    }
+
+    private static Reservation getResultFromResultSet(ResultSet rs) throws SQLException {
+        Reservation reservation;
+        if (!rs.next()) {
+            System.out.println("해당 예약은 존재하지 않습니다.");
+            return null;
+        }
+        reservation = new Reservation(
+                rs.getLong("id"),
+                rs.getDate("date").toLocalDate(),
+                rs.getTime("time").toLocalTime(),
+                rs.getString("name"),
+                new Theme(rs.getString("theme_name"),
+                        rs.getString("theme_desc"),
+                        rs.getInt("theme_price"))
+        );
+
+        return reservation;
+    }
+
+    public void deleteReservationForConsole(Long id) {
+        Connection con = null;
+
+        // 드라이버 연결
+        try {
+            con = DriverManager.getConnection("jdbc:h2:~/test;AUTO_SERVER=true", "sa", "");
+            System.out.println("정상적으로 연결되었습니다.");
+        } catch (SQLException e) {
+            System.err.println("연결 오류:" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            String sql = "DELETE FROM reservation WHERE id = ?;";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setLong(1, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            if (con != null)
+                con.close();
+        } catch (SQLException e) {
+            System.err.println("con 오류:" + e.getMessage());
+        }
     }
 }
