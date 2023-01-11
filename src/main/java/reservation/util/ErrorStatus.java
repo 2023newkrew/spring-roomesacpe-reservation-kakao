baf12c82eaa3a4a693fde96d3ad05ffcf97d7455
kimtaehyun98 <kimtaehyun98@naver.com>
2023-01-11 15:51:50 +0900

    refactor(reservation): status code 404로 변경

--- src/main/java/reservation/util/ErrorStatus.java
+++ src/main/java/reservation/util/ErrorStatus.java
@@ -2,7 +2,7 @@ package reservation.util;
 
 public enum ErrorStatus {
     RESERVATION_DUPLICATED(400, "해당 날짜와 시간에 이미 예약이 존재합니다."),
-    RESERVATION_NOT_FOUND(400, "해당 예약이 존재하지 않습니다.");
+    RESERVATION_NOT_FOUND(404, "해당 예약이 존재하지 않습니다.");
 
 
     private final int status;
