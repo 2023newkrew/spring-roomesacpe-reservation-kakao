f59fba4bf7e2225e99827825e7e26a50a00bae4a
Ethan <rlfalsgh95@naver.com>
2023-01-11 21:08:34 +0900

    feat : getConnection 함수 제거 및 close Null check 추가

--- src/main/java/nextstep/dao/DatabaseUtil.java
+++ src/main/java/nextstep/dao/DatabaseUtil.java
@@ -1,29 +1,12 @@
 package nextstep.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class DatabaseUtil {
-
-    private static final String DB_URL = "jdbc:h2:~/test;AUTO_SERVER=true";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
-
-    private DatabaseUtil(){}
-
-    public static Connection getConnection(){
-        try {
-            return DriverManager.getConnection(DB_URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            return null;
-        }
-    }
-
     public static void close(AutoCloseable... closeables){
         for (AutoCloseable closeable : closeables) {
             try {
-                closeable.close();
+                if(closeable != null){
+                    closeable.close();
+                }
             }catch (Exception exception){
                 exception.printStackTrace();
             }
