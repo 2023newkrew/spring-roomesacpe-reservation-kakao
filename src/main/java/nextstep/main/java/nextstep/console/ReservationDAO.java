793866dc1d48942da76668c647104eaf3b40ca6a
soony.kwonn <soony.kwonn@kakaocorp.com>
2023-01-11 16:06:25 +0900

    refactor: 공통 preparedStatement 분리

--- src/main/java/nextstep/main/java/nextstep/console/ReservationDAO.java
+++ src/main/java/nextstep/main/java/nextstep/console/ReservationDAO.java
@@ -3,6 +3,7 @@ package nextstep.main.java.nextstep.console;
 import nextstep.main.java.nextstep.domain.Reservation;
 import nextstep.main.java.nextstep.domain.Theme;
 import nextstep.main.java.nextstep.repository.ReservationRepository;
+import nextstep.main.java.nextstep.repositoryUtil.ReservationPreparedStatementCreator;
 
 import java.sql.*;
 import java.time.LocalDate;
@@ -17,18 +18,7 @@ public class ReservationDAO implements ReservationRepository {
     public Reservation save(Reservation reservation) {
         connect();
         try {
-            String sql = "INSERT INTO reservation (date, time, name, theme_name, theme_desc, theme_price) VALUES (?, ?, ?, ?, ?, ?);";
-            PreparedStatement preparedStatement = con.prepareStatement(sql, new String[]{"id"});
-            preparedStatement.setDate(1, Date.valueOf(reservation.getDate()));
-            preparedStatement.setTime(2, Time.valueOf(reservation.getTime()));
-            preparedStatement.setString(3, reservation.getName());
-            preparedStatement.setString(4, reservation.getTheme()
-                    .getName());
-            preparedStatement.setString(5, reservation.getTheme()
-                    .getDesc());
-            preparedStatement.setInt(6, reservation.getTheme()
-                    .getPrice());
-
+            PreparedStatement preparedStatement = ReservationPreparedStatementCreator.insertReservationPreparedStatement(con, reservation);
             preparedStatement.executeUpdate();
             return new Reservation(getGeneratedKeys(preparedStatement), reservation);
         } catch (SQLException e) {
--- src/main/java/nextstep/main/java/nextstep/repository/JdbcReservationRepository.java
+++ src/main/java/nextstep/main/java/nextstep/repository/JdbcReservationRepository.java
@@ -2,6 +2,7 @@ package nextstep.main.java.nextstep.repository;
 
 import nextstep.main.java.nextstep.domain.Reservation;
 import nextstep.main.java.nextstep.domain.Theme;
+import nextstep.main.java.nextstep.repositoryUtil.ReservationPreparedStatementCreator;
 import org.springframework.context.annotation.Primary;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,7 +11,6 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
-import java.sql.PreparedStatement;
 import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -31,20 +31,10 @@ public class JdbcReservationRepository implements ReservationRepository {
     @Override
     public Reservation save(Reservation reservation) {
         KeyHolder keyHolder = new GeneratedKeyHolder();
-        String sql = "INSERT INTO reservation (date, time, name, theme_name, theme_desc, theme_price) VALUES (?, ?, ?, ?, ?, ?);";
-        jdbcTemplate.update((connection) -> {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql, new String[]{"id"});
-            preparedStatement.setDate(1, Date.valueOf(reservation.getDate()));
-            preparedStatement.setTime(2, Time.valueOf(reservation.getTime()));
-            preparedStatement.setString(3, reservation.getName());
-            preparedStatement.setString(4, reservation.getTheme()
-                    .getName());
-            preparedStatement.setString(5, reservation.getTheme()
-                    .getDesc());
-            preparedStatement.setInt(6, reservation.getTheme()
-                    .getPrice());
-            return preparedStatement;
-        }, keyHolder);
+
+        jdbcTemplate.update((connection) ->
+                        ReservationPreparedStatementCreator.insertReservationPreparedStatement(connection, reservation)
+                , keyHolder);
         return new Reservation(keyHolder.getKey()
                 .longValue(), reservation);
     }
