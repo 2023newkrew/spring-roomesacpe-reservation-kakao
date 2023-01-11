e20a7d3afa2f9bcc8da434340a59ff8d3eb612b0
엄태호 <qqsscc7342@gmail.com>
2023-01-11 09:41:44 +0900

    refactor: 오버라이딩 되지 않은 메소드 추가

--- src/main/java/nextstep/main/java/nextstep/exception/exception/NoSuchReservationException.java
+++ src/main/java/nextstep/main/java/nextstep/exception/exception/NoSuchReservationException.java
@@ -8,4 +8,16 @@ public class NoSuchReservationException extends RuntimeException {
     public NoSuchReservationException(String message) {
         super(message);
     }
+
+    public NoSuchReservationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NoSuchReservationException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NoSuchReservationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
