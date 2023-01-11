8c37d2dfa33fd16fc10c7327701ff324f852aeaf
HQkim <hyunkyu001@gmail.com>
2023-01-11 12:00:50 +0900

    refactor: 상수명을 대문자로 변경

--- src/main/java/web/exception/NoSuchReservationException.java
+++ src/main/java/web/exception/NoSuchReservationException.java
@@ -2,9 +2,9 @@ package web.exception;
 
 public class NoSuchReservationException extends RuntimeException {
 
-    private static final String message = "해당 예약이 존재하지 않습니다.";
+    private static final String MESSAGE = "해당 예약이 존재하지 않습니다.";
 
     public NoSuchReservationException() {
-        super(NoSuchReservationException.message);
+        super(NoSuchReservationException.MESSAGE);
     }
 }
