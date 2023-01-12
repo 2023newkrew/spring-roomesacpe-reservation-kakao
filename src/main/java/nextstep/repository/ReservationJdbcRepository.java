cf37654ba6dbfee9755981ca1f4344537b712f70
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-12 16:37:58 +0900

    refactor: try-with-resources 적용

--- src/main/java/nextstep/repository/ReservationJdbcRepository.java
+++ src/main/java/nextstep/repository/ReservationJdbcRepository.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class ReservationJdbcRepository implements ReservationRepository{
+public class ReservationJdbcRepository implements ReservationRepository {
 
     private static final String DB_URL = "jdbc:h2:tcp://localhost/~/Projects/h2/db/roomescape;AUTO_SERVER=true";
     private static final String DB_USER = "sa";
@@ -17,128 +17,94 @@ public class ReservationJdbcRepository implements ReservationRepository{
     @Override
     public Reservation save(Reservation reservation) {
         String sql = "INSERT INTO reservation (date, time, name, theme_name, theme_desc, theme_price) VALUES (?, ?, ?, ?, ?, ?);";
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
 
-        try {
-            conn = getConnection();
-            ps = conn.prepareStatement(sql, new String[]{"id"});
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql, new String[]{"id"})) {
 
-            ps.setDate(1, Date.valueOf(reservation.getDate()));
-            ps.setTime(2, Time.valueOf(reservation.getTime()));
-            ps.setString(3, reservation.getName());
-            ps.setString(4, reservation.getTheme().getName());
-            ps.setString(5, reservation.getTheme().getDesc());
-            ps.setInt(6, reservation.getTheme().getPrice());
-
+            setParametersOfSaveSql(ps, reservation);
             ps.executeUpdate();
+            reservation.setId(extractGeneratedReservationId(ps));
 
-            resultSet = ps.getGeneratedKeys();
-            if (!resultSet.next()) {
-                throw new SQLException();
-            }
-
-            reservation.setId(resultSet.getLong("id"));
             return reservation;
         } catch (SQLException e) {
             throw new IllegalStateException(e);
-        } finally {
-            close(conn, ps, resultSet);
         }
     }
 
     @Override
     public Optional<Reservation> findById(Long id) {
         String sql = "SELECT * FROM reservation WHERE id = ?";
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
 
-        try {
-            conn = getConnection();
-            ps = conn.prepareStatement(sql);
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
 
             ps.setLong(1, id);
 
-            resultSet = ps.executeQuery();
-            if (resultSet.next()) {
-                return Optional.of(extractReservation(resultSet));
-            }
-            return Optional.empty();
+            return executeFindByIdQuery(ps);
         } catch (SQLException e) {
             throw new IllegalStateException(e);
-        } finally {
-            close(conn, ps, resultSet);
         }
     }
 
     @Override
     public List<Reservation> findAll() {
         String sql = "SELECT * FROM reservation";
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
 
-        try {
-            conn = getConnection();
-            ps = conn.prepareStatement(sql);
-
-            resultSet = ps.executeQuery();
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet resultSet = ps.executeQuery()) {
 
             return extractReservations(resultSet);
         } catch (SQLException e) {
             throw new IllegalStateException(e);
-        } finally {
-            close(conn, ps, resultSet);
         }
     }
 
     @Override
     public boolean delete(Long id) {
         String sql = "DELETE FROM reservation WHERE id = ?";
-        Connection conn = null;
-        PreparedStatement ps = null;
 
-        try {
-            conn = getConnection();
-            ps = conn.prepareStatement(sql);
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
 
             ps.setLong(1, id);
 
             return ps.executeUpdate() > 0;
-        } catch (SQLException e){
+        } catch (SQLException e) {
             throw new IllegalStateException(e);
-        } finally {
-            close(conn, ps, null);
         }
     }
 
-    private Connection getConnection() throws SQLException{
+    private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
 
-    private void close(Connection conn, PreparedStatement ps, ResultSet resultSet) {
-        if (resultSet != null) {
-            try {
-                resultSet.close();
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
-            }
+    private void setParametersOfSaveSql(PreparedStatement ps, Reservation reservation) throws SQLException {
+        ps.setDate(1, Date.valueOf(reservation.getDate()));
+        ps.setTime(2, Time.valueOf(reservation.getTime()));
+        ps.setString(3, reservation.getName());
+        ps.setString(4, reservation.getTheme().getName());
+        ps.setString(5, reservation.getTheme().getDesc());
+        ps.setInt(6, reservation.getTheme().getPrice());
+    }
+
+    private Long extractGeneratedReservationId(PreparedStatement ps) {
+        try (ResultSet resultSet = ps.getGeneratedKeys()) {
+            resultSet.next();
+            return resultSet.getLong("id");
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
         }
-        if (ps != null) {
-            try {
-                ps.close();
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
+    }
+
+    private Optional<Reservation> executeFindByIdQuery(PreparedStatement ps) {
+        try (ResultSet resultSet = ps.executeQuery()) {
+            if (resultSet.next()) {
+                return Optional.of(extractReservation(resultSet));
             }
-        }
-        if (conn != null) {
-            try {
-                conn.close();
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
-            }
+            return Optional.empty();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
         }
     }
 
