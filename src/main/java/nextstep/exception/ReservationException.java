220fff7c3215701cae7cdb440e4489f4db12635b
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-01-12 15:56:55 +0900

    feat: 예외처리를 위한 ReservationException 및 ErrorCode 구현

---
+++ src/main/java/nextstep/exception/ReservationException.java
@@ -0,0 +1,19 @@
+package nextstep.exception;
+
+public class ReservationException extends RuntimeException {
+    private ErrorCode errorCode;
+    private String errorMessage;
+
+    public ReservationException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorCode.getDescription();
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
