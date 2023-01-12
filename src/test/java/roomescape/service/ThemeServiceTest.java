88a68818537c11d944d3998295e04cece27f56ce
pyun0825 <pyun0825@naver.com>
2023-01-12 16:44:43 +0900

    test: add Theme deletion test

--- src/test/java/roomescape/service/ThemeServiceTest.java
+++ src/test/java/roomescape/service/ThemeServiceTest.java
@@ -6,12 +6,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import roomescape.dto.ReservationRequestDto;
 import roomescape.dto.ThemeRequestDto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @SpringBootTest
 public class ThemeServiceTest {
     @Autowired
     private ThemeService themeService;
+    @Autowired
+    private ReservationService reservationService;
 
     @DisplayName("Theme 저장 후 조회 가능함을 확인")
     @Test
@@ -35,5 +41,18 @@ public class ThemeServiceTest {
         Long themeId = themeService.createTheme(themeRequestDto);
         //then
         Assertions.assertThatIllegalArgumentException().isThrownBy(() -> themeService.createTheme(themeRequestDto));
+    }
+
+    @DisplayName("예약이 있는 theme 지우려하면 예외 발생")
+    @Test
+    @Transactional
+    public void deleteThemeTest() {
+        //given
+        ThemeRequestDto themeRequestDto = new ThemeRequestDto("Test Theme", "Lorem Ipsum", 1000);
+        Long themeId = themeService.createTheme(themeRequestDto);
+        reservationService.createReservation(new ReservationRequestDto(LocalDate.now(), LocalTime.now(), "Tester", themeId));
+        //when
+        //then
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> themeService.deleteTheme(themeId));
     }
 }
