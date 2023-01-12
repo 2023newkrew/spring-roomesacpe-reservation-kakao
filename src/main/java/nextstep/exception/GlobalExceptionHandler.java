0f75cb2589ff9258ed7c05edeaa6cc7bfadde8b1
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-01-12 15:58:37 +0900

    feat: 예외시 API 응답처리를 위한 GlobalExceptionHandler 구현

--- src/main/java/nextstep/exception/GlobalExceptionHandler.java
+++ src/main/java/nextstep/exception/GlobalExceptionHandler.java
@@ -1,15 +1,24 @@
 package nextstep.exception;
 
+import nextstep.dto.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import static nextstep.exception.ErrorCode.INTERNAL_SERVER_ERROR;
 
 @RestController
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(value = ReservationException.class)
+    public ErrorResponse reservationExceptionHandler(ReservationException e) {
+        return new ErrorResponse(e.getErrorCode(), e.getErrorMessage());
+    }
+
+    // 위에서 잡히지 않는 예외들은 모두 INTERNAL_SERVER_ERROR로 응답한다.
     @ExceptionHandler(value = Exception.class)
-    public String exceptionHandler() {
-        return "Exception is occurred.";
+    public ErrorResponse exceptionHandler() {
+        return new ErrorResponse(INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR.getDescription());
     }
 }
