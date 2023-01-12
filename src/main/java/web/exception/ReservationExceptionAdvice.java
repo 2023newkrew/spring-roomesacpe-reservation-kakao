545a2c368176921e5df26bae47bd7ac688bfc086
정핸모 <gusah009@naver.com>
2023-01-12 09:50:06 +0900

    feat: @valid에 대한 예외 처리 추가

--- src/main/java/web/exception/ReservationExceptionAdvice.java
+++ src/main/java/web/exception/ReservationExceptionAdvice.java
@@ -1,11 +1,22 @@
 package web.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ReservationExceptionAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage();
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.from(errorMessage));
+    }
 
     @ExceptionHandler(ReservationException.class)
     public ResponseEntity<ErrorResponse> handleReservationDuplicateException(ReservationException e) {
