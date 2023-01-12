63a0cafbdb6023419e23e660bf2a17b01e4ed235
jerrie.choi <pinkytime96@gmail.com>
2023-01-12 16:37:51 +0900

    refac: 테마 엔티티 추가

--- src/main/java/roomescape/domain/Theme.java
+++ src/main/java/roomescape/domain/Theme.java
@@ -1,14 +1,20 @@
 package roomescape.domain;
 
 public class Theme {
+    private Long id;
     private String name;
     private String desc;
     private Integer price;
 
-    public Theme(String name, String desc, Integer price) {
+    public Theme(Long id, String name, String desc, Integer price) {
+        this.id = id;
         this.name = name;
         this.desc = desc;
         this.price = price;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
