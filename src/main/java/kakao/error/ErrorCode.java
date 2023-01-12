6d9f61bf6c137a06961a00c539b1e6b32c75962f
baker.gus <baker.gus@kakaocorp.com>
2023-01-12 16:41:10 +0900

    refactor: NOT_FOUND 404로 수정

--- src/main/java/kakao/error/ErrorCode.java
+++ src/main/java/kakao/error/ErrorCode.java
@@ -4,7 +4,7 @@ public enum ErrorCode {
     DUPLICATE_RESERVATION(400, "해당 시간에 예약이 존재합니다."),
     DUPLICATE_THEME(400, "해당 이름의 테마가 존재합니다."),
     RESERVATION_NOT_FOUND(400, "해당 ID의 예약이 존재하지 않습니다."),
-    THEME_NOT_FOUND(400, "해당 ID의 테마가 존재하지 않습니다");
+    THEME_NOT_FOUND(404, "해당 ID의 테마가 존재하지 않습니다");
 
     private final int status;
     private final String message;
