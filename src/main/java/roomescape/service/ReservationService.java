6150a2a25b94eac0e986c99a32fa09f62bdf1375
jerrie.choi <pinkytime96@gmail.com>
2023-01-12 17:07:30 +0900

    feat: 예약 생성 시 테마 속성 추가에 따른 변경 사항 수정

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,36 +1,41 @@
 package roomescape.service;
 
 import org.springframework.transaction.annotation.Transactional;
-import roomescape.dao.ReservationDAO;
+import roomescape.repository.ReservationRepository;
+import roomescape.repository.ThemeRepository;
 import roomescape.domain.Reservation;
 import org.springframework.stereotype.Service;
-import roomescape.dao.ReservationAppDAO;
+import roomescape.domain.Theme;
 import roomescape.dto.ReservationRequest;
 
 @Service
 @Transactional
 public class ReservationService {
 
-    private ReservationDAO reservationDAO;
+    private final ReservationRepository reservationRepository;
+    private final ThemeRepository themeRepository;
 
-    public ReservationService(ReservationAppDAO reservationAppDAO) {
-        this.reservationDAO = reservationAppDAO;
+    public ReservationService(ReservationRepository reservationRepository, ThemeRepository themeRepository) {
+        this.reservationRepository = reservationRepository;
+        this.themeRepository = themeRepository;
     }
 
     public Reservation createReservation(ReservationRequest reservationRequest) {
-        if (reservationDAO.checkSchedule(reservationRequest) == 0) {
-            Long id = reservationDAO.addReservation(reservationRequest.toReservation());
-            return reservationDAO.findReservation(id);
+        if (reservationRepository.checkSchedule(reservationRequest) == 0) {
+            Theme theme = themeRepository.findThemeById(reservationRequest.getTheme_id());
+            Long id = reservationRepository.addReservation(reservationRequest.toReservation(theme));
+//            return id;
+            return reservationRepository.findReservation(id);
         }
         return null;
     }
 
     public Reservation showReservation(Long id) {
-        return reservationDAO.findReservation(id);
+        return reservationRepository.findReservation(id);
     }
 
     public int deleteReservation(Long id) {
-        return reservationDAO.removeReservation(id);
+        return reservationRepository.removeReservation(id);
     }
 
 }
