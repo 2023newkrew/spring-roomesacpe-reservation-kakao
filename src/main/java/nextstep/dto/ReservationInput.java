87f6f129c61ee7c5c4177a090d79fadc3cb91232
Nelfunction <oat641@gmail.com>
2023-01-12 13:59:51 +0900

    feat: ReservationInput 추가

---
+++ src/main/java/nextstep/dto/ReservationInput.java
@@ -0,0 +1,80 @@
+package nextstep.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+    private Long id;
+    private LocalDate date;
+    private LocalTime time;
+    private String name;
+    private Theme theme;
+
+    public Reservation() {
+    }
+
+    public Reservation(LocalDate date, LocalTime time, String name) {
+        this.date = date;
+        this.time = time;
+        this.name = name;
+    }
+
+    public Reservation(Long id, LocalDate date, LocalTime time, String name, Theme theme) {
+        this.id = id;
+        this.date = date;
+        this.time = time;
+        this.name = name;
+        this.theme = theme;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTheme(Theme theme) {
+        this.theme = theme;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id=" + id +
+                ", date=" + date +
+                ", time=" + time +
+                ", name='" + name + '\'' +
+                ", theme=" + theme +
+                '}';
+    }
+}
