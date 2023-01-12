37856d06f8f57eb17a3e830f98e19ee2760e7cf9
ekzm8523 <mjw8523@gmail.com>
2023-01-12 22:58:29 +0900

    refactor: RestAPIException 정의

--- src/main/java/nextstep/reservation/exceptions/ReservationControllerAdvice.java
+++ src/main/java/nextstep/reservation/exceptions/ReservationControllerAdvice.java
@@ -1,16 +1,18 @@
 package nextstep.reservation.exceptions;
 
 import nextstep.reservation.controller.ReservationController;
-import nextstep.reservation.exceptions.exception.DuplicateReservationException;
+import nextstep.reservation.exceptions.exception.RestAPIException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice(assignableTypes = {ReservationController.class})
 public class ReservationControllerAdvice {
-    @ExceptionHandler(DuplicateReservationException.class)
-    public ResponseEntity<String> DuplicateReservation() {
-        return ResponseEntity.badRequest().body("해당 시간에 중복된 예약이 있습니다.");
+    @ExceptionHandler(RestAPIException.class)
+    public ResponseEntity<String> handleDuplicatedReservation(RestAPIException e) {
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(e.getResponseMessage());
     }
-
 }
--- src/main/java/nextstep/reservation/exceptions/exception/DuplicateReservationException.java
+++ src/main/java/nextstep/reservation/exceptions/exception/DuplicateReservationException.java
@@ -1,4 +1,18 @@
 package nextstep.reservation.exceptions.exception;
 
-public class DuplicateReservationException extends RuntimeException{
+import org.springframework.http.HttpStatus;
+
+public class DuplicateReservationException extends RestAPIException {
+    public DuplicateReservationException() {
+        this("해당 시간에 중복된 예약이 있습니다.");
+    }
+
+    public DuplicateReservationException(String responseMessage) {
+        super(responseMessage);
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return HttpStatus.BAD_REQUEST;
+    }
 }
