71802b07543f4360b3554fe28a744eec81a62f32
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-01-12 15:57:23 +0900

    feat: 예외시 API 응답처리를 위한 ErrorResponse 클래스 구현

---
+++ src/main/java/nextstep/dto/ErrorResponse.java
@@ -0,0 +1,21 @@
+package nextstep.dto;
+
+import nextstep.exception.ErrorCode;
+
+public class ErrorResponse {
+    private ErrorCode errorCode;
+    private String errorMessage;
+
+    public ErrorResponse(ErrorCode errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
