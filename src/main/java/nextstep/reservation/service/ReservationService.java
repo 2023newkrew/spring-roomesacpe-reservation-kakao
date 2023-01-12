4a49877219cbdd385d1d63d231e926beaf1ea389
ekzm8523 <mjw8523@gmail.com>
2023-01-12 23:49:32 +0900

    refactor: repository 필드 네이밍 변경

--- src/main/java/nextstep/reservation/service/ReservationService.java
+++ src/main/java/nextstep/reservation/service/ReservationService.java
@@ -5,30 +5,43 @@ import lombok.RequiredArgsConstructor;
 import nextstep.reservation.dto.ReservationRequestDto;
 import nextstep.reservation.dto.ReservationResponseDto;
 import nextstep.reservation.entity.Reservation;
+import nextstep.reservation.exceptions.exception.DuplicateReservationException;
 import nextstep.reservation.repository.ReservationRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
-    private final ReservationRepository reservationRepository;
+    private final ReservationRepository repository;
 
     public Long addReservation(final ReservationRequestDto requestDto) {
-        return reservationRepository.add(requestDto.toEntity());
+        if (isDuplicatedReservation(requestDto)) {
+            throw new DuplicateReservationException();
+        }
+        return repository.add(requestDto.toEntity());
     }
 
     public ReservationResponseDto getReservation(final Long id) {
         return new ReservationResponseDto(
-                reservationRepository.findById(id)
+                repository.findById(id)
                         .orElseThrow(IllegalArgumentException::new)
         );
     }
 
     public void deleteReservation(final Long id) {
-        reservationRepository.delete(id);
+        repository.delete(id);
     }
 
     public List<Reservation> getAllReservation() {
-        return reservationRepository.findAll();
+        return repository.findAll();
+    }
+
+    public boolean isDuplicatedReservation(ReservationRequestDto requestDto) {
+        return repository.findAll()
+                .stream()
+                .anyMatch(
+                        reservation -> reservation.getDate().equals(requestDto.getDate())
+                                && reservation.getTime().equals(requestDto.getTime())
+                );
     }
 }
