3f3005e7010b848880a582fd5f855bb491f05645
Subin Min <tnqls5417@naver.com>
2023-01-12 11:19:21 +0900

    refactor: SQL 파일 코멘트 추가
    - 컬럼에 대한 설명 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS RESERVATION
 (
-    id          bigint not null auto_increment,
-    date        date,
-    time        time,
-    name        varchar(20),
-    theme_name  varchar(20),
-    theme_desc  varchar(255),
-    theme_price int,
+    id          bigint not null auto_increment, -- 방탈출 예약 번호
+    date        date, -- 방탈출 예약 날짜
+    time        time, -- 방탈출 예약 시간
+    name        varchar(20), -- 방탈출 예약 이름
+    theme_name  varchar(20), -- 예약된 방탈출 테마 이름
+    theme_desc  varchar(255), -- 예약된 방탈출 테마 설명
+    theme_price int, -- 예약된 방탈출 테마 가격
     primary key (id)
 );
