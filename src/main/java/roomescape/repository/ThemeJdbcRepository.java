a949d4afa2638ba72f1a119da248a5192d5927cc
pyun0825 <pyun0825@naver.com>
2023-01-12 12:16:18 +0900

    refactor: change ThemeRepository and add ThemeJdbcRepository

--- src/main/java/roomescape/repository/ThemeJdbcRepository.java
+++ src/main/java/roomescape/repository/ThemeJdbcRepository.java
@@ -1,11 +1,10 @@
 package roomescape.repository;
 
-import org.springframework.stereotype.Repository;
 import roomescape.model.Theme;
+
 import java.util.Optional;
 
-@Repository
-public class ThemeMockRepository implements ThemeRepository {
+public class ThemeJdbcRepository implements ThemeRepository {
     @Override
     public long save(Theme theme) {
         return 0;
@@ -18,7 +17,7 @@ public class ThemeMockRepository implements ThemeRepository {
 
     @Override
     public Optional<Theme> findOneByName(String themeName) {
-        return Optional.of(new Theme(null, "워너고홈", "병맛 어드벤처 회사 코믹물", 29_000));
+        return Optional.empty();
     }
 
     @Override
--- src/main/java/roomescape/repository/ThemeRepository.java
+++ src/main/java/roomescape/repository/ThemeRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 public interface ThemeRepository {
     long save(Theme theme);
+    Optional<Theme> findOneById(Long themeId);
     Optional<Theme> findOneByName(String themeName);
-    void deleteByName(String themeName);
+    void delete(Long themeId);
 }
