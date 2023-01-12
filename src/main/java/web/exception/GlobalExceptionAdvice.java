51f8ee4c43e87ce52840eb74ab3c249dba40b44d
정핸모 <gusah009@naver.com>
2023-01-12 11:15:12 +0900

    feat: Json LocalDateTime 바인딩 실패 시 bad request 호출

--- src/main/java/web/exception/GlobalExceptionAdvice.java
+++ src/main/java/web/exception/GlobalExceptionAdvice.java
@@ -1,6 +1,7 @@
 package web.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +12,11 @@ public class GlobalExceptionAdvice {
     public ResponseEntity<ErrorResponse> methodArgumentNotValidException(RuntimeException ignore) {
         return ResponseEntity.internalServerError()
                 .body(ErrorResponse.from("서버에 문제가 생겼습니다."));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.from(e.getMessage()));
     }
 }
