128877768a71810a72425dc567992db4710ccb75
yooj929 <jang0929@naver.com>
2023-01-12 13:17:59 +0900

    refactor: properties 직접 parsing

--- src/main/java/nextstep/dto/ConnectionHandler.java
+++ src/main/java/nextstep/dto/ConnectionHandler.java
@@ -1,23 +1,63 @@
 package nextstep.dto;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import org.springframework.beans.factory.annotation.Value;
+
 public class ConnectionHandler {
 
-    @Value("${spring.datasource.hikari.jdbc-url}")
-    private String CONNECTION_URL = "jdbc:h2:tcp://localhost/~/test";
-    @Value("${spring.datasource.hikari.username}")
-    private String USERNAME = "sa";
+    private static String CONNECTION_URL;
 
-    @Value("${spring.datasource.hikari.password}")
-    private String PASSWORD ="";
+    private static String USERNAME;
+
+    private static String PASSWORD;
+
+    static {
+        FileReader reader;
+        try {
+            reader = new FileReader("src/main/resources/application.yml");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.contains("jdbc-url")) {
+                    CONNECTION_URL = parseTokens(line.strip().split(" "));
+                }
+                if (line.contains("username")) {
+                    USERNAME = parseTokens(line.strip().split(" "));
+                }
+                if (line.contains("password")) {
+                    PASSWORD = parseTokens(line.strip().split(" "));
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static String parseTokens(String[] tokens) {
+        if (tokens.length == 1) {
+            return "";
+        }
+        return tokens[1];
+    }
+
+
     private final Connection connection;
 
     public ConnectionHandler() {
         this.connection = connect();
+
     }
 
     private Connection connect() {
