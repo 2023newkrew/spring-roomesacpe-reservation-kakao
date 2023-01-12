2245e495f7d6156549cb1f17fa0e83f4f2ca98fc
MibSang <secured8372@gmail.com>
2023-01-12 14:13:14 +0900

    test: 테스트 후 해당 column 이 삭제될 수 있도록 수정

--- src/test/java/nextstep/web/controller/ReservationControllerTest.java
+++ src/test/java/nextstep/web/controller/ReservationControllerTest.java
@@ -1,20 +1,23 @@
 package nextstep.web.controller;
 
 import io.restassured.RestAssured;
+import nextstep.domain.Reservation;
+import nextstep.web.common.repository.RoomEscapeRepository;
 import nextstep.web.reservation.dto.CreateReservationRequestDto;
-import nextstep.web.reservation.dto.CreateReservationResponseDto;
-import nextstep.web.reservation.service.ReservationService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import static org.hamcrest.Matchers.*;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ReservationControllerTest {
 
@@ -22,7 +25,7 @@ class ReservationControllerTest {
     int port;
 
     @Autowired
-    ReservationService reservationService;
+    RoomEscapeRepository<Reservation> reservationDao;
 
     @BeforeEach
     void setUp() {
@@ -30,43 +33,31 @@ class ReservationControllerTest {
     }
 
     @Test
-    void 예약을_생성할_수_있다() {
+    @Transactional
+    void 예약_정상_생성_조회_삭제() {
         CreateReservationRequestDto requestDto = new CreateReservationRequestDto(
-                LocalDate.of(2023, 1, 10), LocalTime.of(13, 0), "reservation1", 1L
+                LocalDate.of(2023, 1, 10), LocalTime.of(13, 0), "tester", 1L
         );
 
-        RestAssured.given().log().all()
+        String location = RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(requestDto)
-                .when().post("/reservations")
+                .when().post(Reservation.BASE_URL)
                 .then().log().all()
-                .statusCode(HttpStatus.OK.value());
-    }
-
-    @Test
-    void 예약을_조회할_수_있다() {
-        CreateReservationRequestDto requestDto = new CreateReservationRequestDto(
-                LocalDate.of(2023, 1, 10), LocalTime.of(13, 0), "reservation1", 1L
-        );
-        CreateReservationResponseDto responseDto = reservationService.createReservation(requestDto);
+                .statusCode(HttpStatus.OK.value())
+                .body("data.location", containsString(Reservation.BASE_URL))
+                .extract().body().jsonPath().get("data.location");
 
         RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get(responseDto.getLocation())
+                .when().get(location)
                 .then().log().all()
-                .statusCode(HttpStatus.OK.value());
-    }
-
-    @Test
-    void 예약을_취소할_수_있다() {
-        CreateReservationRequestDto requestDto = new CreateReservationRequestDto(
-                LocalDate.of(2023, 1, 10), LocalTime.of(13, 0), "reservation1", 1L
-        );
-        CreateReservationResponseDto responseDto = reservationService.createReservation(requestDto);
+                .statusCode(HttpStatus.OK.value())
+                .body("data.name", equalTo("tester"));
 
         RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().delete(responseDto.getLocation())
+                .when().delete(location)
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value());
     }
