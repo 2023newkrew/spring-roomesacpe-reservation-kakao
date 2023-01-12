a0620211740fd999ec693c7851619ab3a37faf5e
gh1224 <rlarkgus0@naver.com>
2023-01-12 13:55:16 +0900

    refactor: controller의 복잡한 로직은 service에 분리

--- src/main/java/nextstep/controller/ReservationController.java
+++ src/main/java/nextstep/controller/ReservationController.java
@@ -3,33 +3,26 @@ package nextstep.controller;
 import nextstep.dao.ReservationDAO;
 import nextstep.domain.Reservation;
 import nextstep.domain.ReservationInfo;
-import nextstep.domain.Theme;
-import nextstep.exceptions.DataConflictException;
+import nextstep.service.ReservationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
+    private final ReservationService reservationService;
     private final ReservationDAO reservationDAO;
-    private final Theme theme = new Theme("워너고홈", "병맛 어드벤처 회사 코믹물", 29_000);
 
-    public ReservationController(ReservationDAO reservationJdbcTemplateDAO) {
+    public ReservationController(ReservationService reservationService, ReservationDAO reservationJdbcTemplateDAO) {
+        this.reservationService = reservationService;
         this.reservationDAO = reservationJdbcTemplateDAO;
     }
 
     @PostMapping("")
     public ResponseEntity createReservation(@RequestBody Reservation reservation) {
-        List<Reservation> reservationsByDateAndTime = reservationDAO.findByDateAndTime(reservation.getDate(), reservation.getTime());
-        if (reservationsByDateAndTime.size() > 0) {
-            throw new DataConflictException("동일한 시간대에 예약이 이미 존재합니다.");
-        }
-
-        Reservation newReservation = new Reservation(reservation.getDate(), reservation.getTime(), reservation.getName(), theme);
-        Long id = reservationDAO.save(newReservation);
+        Long id = reservationService.saveReservation(reservation);
         return ResponseEntity.created(URI.create("/reservations/" + id)).build();
     }
 
