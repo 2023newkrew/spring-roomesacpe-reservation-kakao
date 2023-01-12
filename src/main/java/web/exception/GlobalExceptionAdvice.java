afbc7b2376a2fdbaddc9b68e6a191793fa8efdc5
정핸모 <gusah009@naver.com>
2023-01-12 14:13:26 +0900

    refactor: MethodArgumentNotValidException handler 글로벌 advice로 이동

--- src/main/java/web/exception/GlobalExceptionAdvice.java
+++ src/main/java/web/exception/GlobalExceptionAdvice.java
@@ -2,6 +2,7 @@ package web.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +13,16 @@ public class GlobalExceptionAdvice {
     public ResponseEntity<ErrorResponse> methodArgumentNotValidException(RuntimeException ignore) {
         return ResponseEntity.internalServerError()
                 .body(ErrorResponse.from("서버에 문제가 생겼습니다."));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage();
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.from(errorMessage));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
--- src/main/java/web/reservation/exception/ReservationExceptionAdvice.java
+++ src/main/java/web/reservation/exception/ReservationExceptionAdvice.java
@@ -3,7 +3,6 @@ package web.reservation.exception;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import web.exception.ErrorResponse;
@@ -11,16 +10,6 @@ import web.exception.ErrorResponse;
 @RestControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ReservationExceptionAdvice {
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
-        String errorMessage = e.getBindingResult()
-                .getAllErrors()
-                .get(0)
-                .getDefaultMessage();
-        return ResponseEntity.badRequest()
-                .body(ErrorResponse.from(errorMessage));
-    }
 
     @ExceptionHandler(ReservationException.class)
     public ResponseEntity<ErrorResponse> handleReservationDuplicateException(ReservationException e) {
