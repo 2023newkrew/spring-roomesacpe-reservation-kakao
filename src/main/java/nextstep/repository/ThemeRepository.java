42dac544578b6a40646b1555d52cf1440a6b7311
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-01-13 16:44:55 +0900

    feat: ThemeRepository 인터페이스 구현

---
+++ src/main/java/nextstep/repository/ThemeRepository.java
@@ -0,0 +1,18 @@
+package nextstep.repository;
+
+import nextstep.domain.Reservation;
+import nextstep.domain.Theme;
+
+public interface ThemeRepository {
+    Long save(String name, String desc, Integer price);
+
+    Long save(Theme theme);
+
+    Reservation findById(Long id);
+
+    void deleteById(Long id);
+
+    void createTable();
+
+    void dropTable();
+}
