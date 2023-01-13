f122eefebde0c70b5c04be2377f963de92a7cc9c
Siryeong <siryeongchoi@gmail.com>
2023-01-13 23:08:19 +0900

    test: 예약된 테마를 삭제하면 예외가 발생한다.

--- src/test/java/nextstep/acceptance/ThemeAcceptanceTest.java
+++ src/test/java/nextstep/acceptance/ThemeAcceptanceTest.java
@@ -3,13 +3,16 @@ package nextstep.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.dto.CreateReservationRequest;
 import nextstep.dto.CreateThemeRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.hamcrest.Matchers.equalTo;
 
@@ -20,9 +23,32 @@ public class ThemeAcceptanceTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
+        jdbcTemplate.execute("DROP TABLE reservation IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE RESERVATION\n" +
+                "(\n" +
+                "    id          bigint not null auto_increment,\n" +
+                "    date        date,\n" +
+                "    time        time,\n" +
+                "    name        varchar(20),\n" +
+                "    theme_id    bigint not null,\n" +
+                "    primary key (id)\n" +
+                ");");
+
+        jdbcTemplate.execute("DROP TABLE theme IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE theme\n" +
+                "(\n" +
+                "    id    bigint not null auto_increment,\n" +
+                "    name  varchar(20),\n" +
+                "    desc  varchar(255),\n" +
+                "    price int,\n" +
+                "    primary key (id)\n" +
+                ");");
     }
 
     @Test
@@ -112,6 +138,32 @@ public class ThemeAcceptanceTest {
                 .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 
+    @Test
+    void 예약된_테마를_삭제하면_예외가_발생한다() {
+        // given
+        CreateThemeRequest themeRequest = new CreateThemeRequest("테마1", "테마내용입니다", 19000);
+        ExtractableResponse<Response> response = createTheme(themeRequest);
+        Long themeId = Long.parseLong(response.header("Location").split("/")[2]);
+
+        CreateReservationRequest reservationRequest = new CreateReservationRequest(
+                "2022-02-02",
+                "13:00",
+                "davi",
+                themeId);
+        createReservation(reservationRequest);
+
+        RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+
+        // when
+                .when().delete("/themes/" + themeId)
+
+        // then
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
     private ExtractableResponse<Response> createTheme(CreateThemeRequest themeRequest) {
         return RestAssured
                 .given()
@@ -119,6 +171,17 @@ public class ThemeAcceptanceTest {
                 .body(themeRequest)
                 .when()
                 .post("/themes")
+                .then()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> createReservation(CreateReservationRequest createReservationRequest) {
+        return RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(createReservationRequest)
+                .when()
+                .post("/reservations")
                 .then()
                 .extract();
     }
