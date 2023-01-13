fe5473b8a3eb20f686e470f585987b731713227f
MibSang <secured8372@gmail.com>
2023-01-13 10:11:47 +0900

    fix: Json 파싱 예외시 CommenErrorCode로 처리할 수 있도록 수정(handleHttpMessageException)

--- src/main/java/nextstep/web/common/exception/handler/ReservationAdvice.java
+++ src/main/java/nextstep/web/common/exception/handler/ReservationAdvice.java
@@ -5,6 +5,7 @@ import nextstep.web.common.exception.CommonErrorCode;
 import nextstep.web.common.exception.ErrorCode;
 import nextstep.web.common.exception.ErrorResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,7 +27,7 @@ public class ReservationAdvice {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
-        System.out.println(e.getMessage());
+        System.out.println(e.toString());
         ErrorCode errorCode = CommonErrorCode.SERVER_ERROR;
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
@@ -48,5 +49,15 @@ public class ReservationAdvice {
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
                 .body(new ErrorResponse(errorCode.getHttpStatus().value(), message));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageException(HttpMessageNotReadableException e) {
+        System.out.println(e.getMessage());
+        ErrorCode errorCode = CommonErrorCode.BAD_PARAMETER_REQUEST;
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(new ErrorResponse(errorCode.getHttpStatus().value(), errorCode.getMessage()));
     }
 }
