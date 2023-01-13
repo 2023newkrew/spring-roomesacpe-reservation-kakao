9a33d17f4900b96473899f95e0028d26031b4c19
yooj929 <jang0929@naver.com>
2023-01-13 14:12:15 +0900

    refactor: 의존성 주입 datasource대신 jdbctemplate으로 변경

--- src/main/java/nextstep/config/ThemeConfig.java
+++ src/main/java/nextstep/config/ThemeConfig.java
@@ -5,18 +5,27 @@ import nextstep.repository.ThemeRepository;
 import nextstep.repository.ThemeRepositoryImpl;
 import nextstep.service.ThemeService;
 import nextstep.service.ThemeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class ThemeConfig {
-    @Bean
-    ThemeRepository themeRepository(DataSource dataSource){
-        return new ThemeRepositoryImpl(dataSource);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ThemeConfig(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Bean
-    ThemeService themeService(DataSource dataSource){
-        return new ThemeServiceImpl(themeRepository(dataSource));
+    ThemeRepository themeRepository() {
+        return new ThemeRepositoryImpl(jdbcTemplate);
+    }
+
+    @Bean
+    ThemeService themeService() {
+        return new ThemeServiceImpl(themeRepository());
     }
 }
