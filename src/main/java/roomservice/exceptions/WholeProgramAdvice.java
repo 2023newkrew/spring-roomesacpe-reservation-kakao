7b9b9d1ea658a86f4e36f3f97cb37359c698c70d
Minsu Kim <ravor8878@gmail.com>
2023-01-13 15:02:17 +0900

    feat : Spring에서 새로 생긴 예외 처리

--- src/main/java/roomservice/exceptions/WholeProgramAdvice.java
+++ src/main/java/roomservice/exceptions/WholeProgramAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import roomservice.exceptions.exception.DuplicatedReservationException;
+import roomservice.exceptions.exception.DuplicatedThemeNameException;
 import roomservice.exceptions.exception.InvalidReservationTimeException;
 import roomservice.exceptions.exception.InvalidThemeIdException;
 
@@ -15,6 +16,7 @@ import roomservice.exceptions.exception.InvalidThemeIdException;
 public class WholeProgramAdvice {
     /**
      * Handles {@link roomservice.exceptions.exception.DuplicatedReservationException} on whole program.
+     *
      * @return "bad request" with some message.
      */
     @ExceptionHandler(DuplicatedReservationException.class)
@@ -24,28 +26,41 @@ public class WholeProgramAdvice {
 
     /**
      * Handles {@link org.springframework.web.bind.MethodArgumentNotValidException} on whole program.
+     *
      * @return "bad request" with some message.
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity handleMethodArgumentNotValidException(){
+    public ResponseEntity handleMethodArgumentNotValidException() {
         return ResponseEntity.badRequest().body("올바른 값을 입력하세요.");
     }
 
     /**
      * Handles {@link roomservice.exceptions.exception.InvalidReservationTimeException} on whole program.
+     *
      * @return "bad request" with some message.
      */
     @ExceptionHandler(InvalidReservationTimeException.class)
-    public ResponseEntity handleInvalidReservationTimeException(){
+    public ResponseEntity handleInvalidReservationTimeException() {
         return ResponseEntity.badRequest().body("시간표 내의 시각을 입력해 주세요.");
     }
 
     /**
      * Handles {@link roomservice.exceptions.exception.InvalidReservationTimeException} on whole program.
+     *
      * @return "bad request" with some message.
      */
     @ExceptionHandler(InvalidThemeIdException.class)
-    public ResponseEntity handleInvalidIdThemeException(){
+    public ResponseEntity handleInvalidIdThemeException() {
         return ResponseEntity.badRequest().body("올바른 테마 id를 입력해 주세요.");
+    }
+
+    /**
+     * Handles {@link roomservice.exceptions.exception.DuplicatedThemeNameException} on whole program.
+     *
+     * @return "bad request" with some message.
+     */
+    @ExceptionHandler(DuplicatedThemeNameException.class)
+    public ResponseEntity handleDuplicatedThemeNameException() {
+        return ResponseEntity.badRequest().body("이미 존재하는 테마 이름입니다.");
     }
 }
