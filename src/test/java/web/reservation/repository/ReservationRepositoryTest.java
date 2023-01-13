50963e81c1aa181c7720884208bc6849ff5e2190
정핸모 <gusah009@naver.com>
2023-01-13 17:41:29 +0900

    test: FindByThemeId repository TC 추가

--- src/test/java/web/reservation/repository/ReservationRepositoryTest.java
+++ src/test/java/web/reservation/repository/ReservationRepositoryTest.java
@@ -8,9 +8,7 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import web.entity.Reservation;
 import web.entity.Theme;
-import web.exception.ErrorCode;
 import web.reservation.exception.ReservationException;
-import web.theme.exception.ThemeException;
 import web.theme.repository.ThemeRepository;
 
 import javax.sql.DataSource;
@@ -18,6 +16,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -107,6 +106,46 @@ public class ReservationRepositoryTest {
         @Test
         void should_throwException_when_notExistReservation() {
             assertThat(reservationRepository.findById(-1)).isEmpty();
+        }
+
+        @Nested
+        class FindByThemeId {
+
+            @Test
+            void should_returnList_when_multiReservation() {
+                LocalDate today = LocalDate.now();
+                LocalTime now = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
+                String name = "name";
+                Reservation reservation = Reservation.of(null, today, now, name, savedThemeId);
+                long reservationId = reservationRepository.save(reservation);
+
+                LocalDate tomorrow = LocalDate.now().plusDays(1);
+                LocalTime now2 = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
+                String name2 = "name";
+                Reservation reservation2 = Reservation.of(null, tomorrow, now2, name2, savedThemeId);
+                long reservationId2 = reservationRepository.save(reservation2);
+
+                List<Reservation> findReservations = reservationRepository.findAllByThemeId(savedThemeId);
+
+                assertThat(findReservations.get(0).getId()).isEqualTo(reservationId);
+                assertThat(findReservations.get(0).getName()).isEqualTo(name);
+                assertThat(findReservations.get(0).getDate()).isEqualTo(today);
+                assertThat(findReservations.get(0).getTime()).isEqualTo(now);
+                assertThat(findReservations.get(0).getThemeId()).isEqualTo(savedThemeId);
+                assertThat(findReservations.get(1).getId()).isEqualTo(reservationId2);
+                assertThat(findReservations.get(1).getName()).isEqualTo(name2);
+                assertThat(findReservations.get(1).getDate()).isEqualTo(tomorrow);
+                assertThat(findReservations.get(1).getTime()).isEqualTo(now2);
+                assertThat(findReservations.get(1).getThemeId()).isEqualTo(savedThemeId);
+            }
+
+            @Test
+            void should_returnEmptyList_when_notExistTheme() {
+                List<Reservation> findReservations = reservationRepository.findAllByThemeId(-1L);
+
+                assertThat(findReservations).isNotNull();
+                assertThat(findReservations).isEmpty();
+            }
         }
     }
 
