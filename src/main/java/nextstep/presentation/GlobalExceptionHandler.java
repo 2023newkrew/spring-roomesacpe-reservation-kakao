0ed9e64cfb00c5d71a2e1647af946bc84c3e8e9c
Siryeong <siryeongchoi@gmail.com>
2023-01-13 21:58:15 +0900

    feat: 예약 생성 시 테마번호 유효성 검사 - 테마 번호가 기입 되었는지 확인 - 테마 존재 유무 확인

--- src/main/java/nextstep/presentation/GlobalExceptionHandler.java
+++ src/main/java/nextstep/presentation/GlobalExceptionHandler.java
@@ -1,10 +1,7 @@
 package nextstep.presentation;
 
 import nextstep.dto.ErrorResponse;
-import nextstep.exception.DuplicateReservationException;
-import nextstep.exception.InvalidCreateReservationRequestException;
-import nextstep.exception.JdbcException;
-import nextstep.exception.ReservationNotFoundException;
+import nextstep.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -31,6 +28,14 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidCreateReservationRequestException.class)
     public ResponseEntity<ErrorResponse> handleInvalidCreateReservationRequestException(InvalidCreateReservationRequestException e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(ThemeNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleThemeNotFoundException(ThemeNotFoundException e) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
