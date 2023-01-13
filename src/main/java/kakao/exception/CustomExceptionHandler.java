03fd42e31d0e4d39093a947a7e8f4b539d58aa87
BaeRoNuI <xogh20321@gmail.com>
2023-01-13 13:15:35 +0900

    refactor: remote Entity postfix from ErrorResponse

--- src/main/java/kakao/exception/CustomExceptionHandler.java
+++ src/main/java/kakao/exception/CustomExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class CustomExceptionHandler {
     @ExceptionHandler(CustomRuntimeException.class)
-    protected ResponseEntity<ErrorResponseEntity> handleCustomException(CustomRuntimeException e) {
-        return ErrorResponseEntity.toResponseEntity(e.getErrorCode());
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomRuntimeException e) {
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
     }
 }
--- src/main/java/kakao/exception/ErrorResponse.java
+++ src/main/java/kakao/exception/ErrorResponse.java
@@ -6,13 +6,13 @@ import org.springframework.http.ResponseEntity;
 
 @Data
 @Builder
-public class ErrorResponseEntity {
+public class ErrorResponse {
     private int status;
     private String code;
     private String message;
 
-    public static ResponseEntity<ErrorResponseEntity> toResponseEntity(ErrorCode errorCode) {
-        ErrorResponseEntity responseBody = ErrorResponseEntity.builder()
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        ErrorResponse responseBody = ErrorResponse.builder()
                 .status(errorCode.getHttpStatus().value())
                 .code(errorCode.name())
                 .message(errorCode.getMessage())
