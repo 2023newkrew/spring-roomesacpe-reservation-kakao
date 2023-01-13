61f43259c260472a19fec5d808942fe15f07ee25
baker.gus <baker.gus@kakaocorp.com>
2023-01-13 20:21:31 +0900

    refactor: abstract class 생성자 protected로 수정

--- src/main/java/kakao/error/exception/CustomRuntimeException.java
+++ src/main/java/kakao/error/exception/CustomRuntimeException.java
@@ -3,7 +3,7 @@ package kakao.error.exception;
 import kakao.error.ErrorCode;
 
 public abstract class CustomRuntimeException extends RuntimeException {
-    public CustomRuntimeException(ErrorCode errorCode, Throwable cause) {
+    protected CustomRuntimeException(ErrorCode errorCode, Throwable cause) {
         super(errorCode.getMessage(), cause);
     }
 
