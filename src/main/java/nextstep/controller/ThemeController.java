ebf833e086776b28f348268d4bd0a70ae02b19b8
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2023-01-13 15:45:08 +0900

    feat: Theme 컨트롤러와 테스트 추가

---
+++ src/main/java/nextstep/controller/ThemeController.java
@@ -0,0 +1,42 @@
+package nextstep.controller;
+
+import java.net.URI;
+import java.util.List;
+import nextstep.domain.Theme;
+import nextstep.service.ThemeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/themes")
+public class ThemeController {
+    private final ThemeService themeService;
+
+    public ThemeController(ThemeService themeService) {
+        this.themeService = themeService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity createTheme(@RequestBody Theme theme) {
+        Long id = themeService.createTheme(theme);
+        return ResponseEntity.created(URI.create("/themes/" + id)).build();
+    }
+
+    @GetMapping("")
+    public ResponseEntity lookupAllThemes() {
+        List<Theme> themes = themeService.lookupAllThemes();
+        return ResponseEntity.ok().body(themes);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteTheme(@PathVariable Long id) {
+        Theme theme = themeService.deleteTheme(id);
+        return ResponseEntity.ok().body(theme);
+    }
+}
