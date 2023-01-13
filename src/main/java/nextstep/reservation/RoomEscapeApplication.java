1454beee8edddf6fc4a77ebeac66c579002bdea8
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-01-13 15:25:21 +0900

    refactor: Console Application 주석 제거, 변경된 도메인에 맞게 일부 수정 - THEME 정보를 추가할 수 있는 로직 필요 - 조회시 THEME ID만 반환 하는 부분 수정 필요

--- src/main/java/nextstep/reservation/RoomEscapeApplication.java
+++ src/main/java/nextstep/reservation/RoomEscapeApplication.java
@@ -1,98 +1,99 @@
-//package nextstep.reservation;
-//
-//import nextstep.reservation.entity.Reservation;
-//import nextstep.reservation.entity.Theme;
-//import nextstep.reservation.repository.JdbcReservationRepository;
-//import nextstep.reservation.service.ReservationService;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.datasource.DriverManagerDataSource;
-//
-//import javax.sql.DataSource;
-//import java.time.LocalDate;
-//import java.time.LocalTime;
-//import java.util.Scanner;
-//
-//public class RoomEscapeApplication {
-//    private static final String ADD = "add";
-//    private static final String FIND = "find";
-//    private static final String DELETE = "delete";
-//    private static final String QUIT = "quit";
-//
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        ReservationService reservationService = new ReservationService(new JdbcReservationRepository(new JdbcTemplate(dataSource())));
-//
-//
-//        Theme theme = new Theme("워너고홈", "병맛 어드벤처 회사 코믹물", 29_000);
-//
-//        while (true) {
-//            System.out.println();
-//            System.out.println("### 명령어를 입력하세요. ###");
-//            System.out.println("- 예약하기: add {date},{time},{name} ex) add 2022-08-11,13:00,류성현");
-//            System.out.println("- 예약조회: find {id} ex) find 1");
-//            System.out.println("- 예약취소: delete {id} ex) delete 1");
-//            System.out.println("- 종료: quit");
-//
-//            String input = scanner.nextLine();
-//            if (input.startsWith(ADD)) {
-//                String params = input.split(" ")[1];
-//
-//                String date = params.split(",")[0];
-//                String time = params.split(",")[1];
-//                String name = params.split(",")[2];
-//
-//                Reservation reservation = new Reservation(
-//                        null,
-//                        LocalDate.parse(date),
-//                        LocalTime.parse(time + ":00"),
-//                        name,
-//                        theme
-//                );
-//                Reservation createReservation = reservationService.create(reservation);
-//
-//                System.out.println("예약이 등록되었습니다.");
-//                System.out.println("예약 번호: " + createReservation.getId());
-//                System.out.println("예약 날짜: " + createReservation.getDate());
-//                System.out.println("예약 시간: " + createReservation.getTime());
-//                System.out.println("예약자 이름: " + createReservation.getName());
-//            }
-//
-//            if (input.startsWith(FIND)) {
-//                String params = input.split(" ")[1];
-//                long id = Long.parseLong(params.split(",")[0]);
-//                Reservation reservation = reservationService.findById(id);
-//
-//                System.out.println("예약 번호: " + reservation.getId());
-//                System.out.println("예약 날짜: " + reservation.getDate());
-//                System.out.println("예약 시간: " + reservation.getTime());
-//                System.out.println("예약자 이름: " + reservation.getName());
+package nextstep.reservation;
+
+import nextstep.reservation.entity.Reservation;
+import nextstep.reservation.entity.Theme;
+import nextstep.reservation.repository.JdbcReservationRepository;
+import nextstep.reservation.service.ReservationService;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class RoomEscapeApplication {
+    private static final String ADD = "add";
+    private static final String FIND = "find";
+    private static final String DELETE = "delete";
+    private static final String QUIT = "quit";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ReservationService reservationService = new ReservationService(new JdbcReservationRepository(new JdbcTemplate(dataSource())));
+
+
+        Theme theme = new Theme(1L, "워너고홈", "병맛 어드벤처 회사 코믹물", 29_000);
+
+        while (true) {
+            System.out.println();
+            System.out.println("### 명령어를 입력하세요. ###");
+            System.out.println("- 예약하기: add {date},{time},{name} ex) add 2022-08-11,13:00,류성현");
+            System.out.println("- 예약조회: find {id} ex) find 1");
+            System.out.println("- 예약취소: delete {id} ex) delete 1");
+            System.out.println("- 종료: quit");
+
+            String input = scanner.nextLine();
+            if (input.startsWith(ADD)) {
+                String params = input.split(" ")[1];
+
+                String date = params.split(",")[0];
+                String time = params.split(",")[1];
+                String name = params.split(",")[2];
+
+                Reservation reservation = new Reservation(
+                        null,
+                        LocalDate.parse(date),
+                        LocalTime.parse(time + ":00"),
+                        name,
+                        theme.getId()
+                );
+                Reservation createReservation = reservationService.create(reservation);
+
+                System.out.println("예약이 등록되었습니다.");
+                System.out.println("예약 번호: " + createReservation.getId());
+                System.out.println("예약 날짜: " + createReservation.getDate());
+                System.out.println("예약 시간: " + createReservation.getTime());
+                System.out.println("예약자 이름: " + createReservation.getName());
+            }
+
+            if (input.startsWith(FIND)) {
+                String params = input.split(" ")[1];
+                long id = Long.parseLong(params.split(",")[0]);
+                Reservation reservation = reservationService.findById(id);
+
+                System.out.println("예약 번호: " + reservation.getId());
+                System.out.println("예약 날짜: " + reservation.getDate());
+                System.out.println("예약 시간: " + reservation.getTime());
+                System.out.println("예약자 이름: " + reservation.getName());
+                System.out.println("예약 테마 ID: " + reservation.getThemeId());
 //                System.out.println("예약 테마 이름: " + reservation.getTheme().getName());
 //                System.out.println("예약 테마 설명: " + reservation.getTheme().getDesc());
 //                System.out.println("예약 테마 가격: " + reservation.getTheme().getPrice());
-//            }
-//
-//            if (input.startsWith(DELETE)) {
-//                String params = input.split(" ")[1];
-//
-//                long id = Long.parseLong(params.split(",")[0]);
-//
-//                if (reservationService.delete(id)) {
-//                    System.out.println("예약이 취소되었습니다.");
-//                }
-//            }
-//
-//            if (input.equals(QUIT)) {
-//                break;
-//            }
-//        }
-//    }
-//
-//    public static DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setUsername("sa");
-//        dataSource.setPassword("");
-//        dataSource.setDriverClassName("org.h2.Driver");
-//        dataSource.setUrl("jdbc:h2:~/test");
-//        return dataSource;
-//    }
-//}
+            }
+
+            if (input.startsWith(DELETE)) {
+                String params = input.split(" ")[1];
+
+                long id = Long.parseLong(params.split(",")[0]);
+
+                if (reservationService.delete(id)) {
+                    System.out.println("예약이 취소되었습니다.");
+                }
+            }
+
+            if (input.equals(QUIT)) {
+                break;
+            }
+        }
+    }
+
+    public static DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:~/test");
+        return dataSource;
+    }
+}
