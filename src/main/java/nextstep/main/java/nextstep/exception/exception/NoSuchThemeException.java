909d941ddb2cab7f8e7ea97ecdc39c9c8fb35dec
soony.kwonn <soony.kwonn@kakaocorp.com>
2023-01-13 11:08:31 +0900

    feat: 예약 조회 실패 예외 추가

---
+++ src/main/java/nextstep/main/java/nextstep/exception/exception/NoSuchThemeException.java
@@ -0,0 +1,11 @@
+package nextstep.main.java.nextstep.exception.exception;
+
+public class NoSuchThemeException extends RuntimeException{
+    public NoSuchThemeException() {
+        super();
+    }
+
+    public NoSuchThemeException(String message) {
+        super(message);
+    }
+}
