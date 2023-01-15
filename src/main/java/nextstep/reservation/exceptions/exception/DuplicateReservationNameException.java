4c36a4a69e31590755c1945119db2b5ad1df4a63
ekzm8523 <mjw8523@gmail.com>
2023-01-15 23:41:07 +0900

    fix: 오타 수정

--- src/main/java/nextstep/reservation/exceptions/exception/DuplicateReservationNameException.java
+++ src/main/java/nextstep/reservation/exceptions/exception/DuplicateReservationNameException.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 public class DuplicateReservationNameException extends RestAPIException{
 
     public DuplicateReservationNameException() {
-        this("예약이름은 중복될 수 있습니다");
+        this("예약이름은 중복될 수 있습니다.");
     }
 
     public DuplicateReservationNameException(String responseMessage) {
