1c36d5d9d392b078dcf40958ff713f1a1e4b6bf1
Ethan <rlfalsgh95@naver.com>
2023-01-16 02:29:47 +0900

    feat : themeReservationService에서 exception throw 하도록 수정

--- src/main/java/nextstep/reservation/service/ThemeReservationServiceImpl.java
+++ src/main/java/nextstep/reservation/service/ThemeReservationServiceImpl.java
@@ -1,12 +1,13 @@
 package nextstep.reservation.service;
 
 import lombok.RequiredArgsConstructor;
+import nextstep.exception.ConstraintViolationException;
 import nextstep.exception.EntityNotFoundException;
 import nextstep.reservation.dao.ThemeReservationDao;
 import nextstep.reservation.dto.ReservationDetail;
 import nextstep.reservation.dto.ReservationDto;
 import nextstep.reservation.entity.Reservation;
-import nextstep.reservation.exception.ThemeExceptionErrorCode;
+import nextstep.reservation.exception.ThemeReservationErrorCode;
 import nextstep.theme.dao.ThemeDao;
 import nextstep.theme.entity.Theme;
 import nextstep.theme.exception.ThemeErrorCode;
@@ -26,23 +27,33 @@ public class ThemeReservationServiceImpl implements ThemeReservationService {
     @Override
     public Long reserve(ReservationDto reservationDto){
         reservationDto.setThemeId(DEFAULT_THEME_ID);
+
+        if(isExistSameDatetimeReservation(reservationDto)){
+            throw new ConstraintViolationException(ThemeReservationErrorCode.CANNOT_RESERVE_FOR_SAME_DATETIME);
+        }
         Reservation reservation = ReservationDto.from(reservationDto);
+
         themeReservationDao.insert(reservation);
         return reservation.getId();
+    }
+
+    private boolean isExistSameDatetimeReservation(ReservationDto reservationDto){
+        return themeReservationDao.findByDatetime(reservationDto.getDate(), reservationDto.getTime())
+                .isPresent();
     }
 
     @Override
     public void cancelById(Long id){
         int deleteCount = themeReservationDao.delete(id);
         if(deleteCount == 0){
-            throw new EntityNotFoundException(ThemeExceptionErrorCode.RESERVATION_NOT_FOUND);
+            throw new EntityNotFoundException(ThemeReservationErrorCode.RESERVATION_NOT_FOUND);
         }
     }
 
     @Override
     public Optional<ReservationDetail> findById(Long id){
         Reservation reservation = themeReservationDao.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(ThemeExceptionErrorCode.RESERVATION_NOT_FOUND));
+                .orElseThrow(() -> new EntityNotFoundException(ThemeReservationErrorCode.RESERVATION_NOT_FOUND));
 
         Theme theme = themeDao.findById(reservation.getThemeId())
                 .orElseThrow(() -> new EntityNotFoundException(ThemeErrorCode.THEME_NOT_FOUND));
