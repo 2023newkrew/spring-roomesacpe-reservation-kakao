e0531aece093355ec77848026eade8635c018ec6
Neurumaru <ksj6659@gmail.com>
2023-01-15 22:56:43 +0900

    test(ThemeControllerExceptionTest): 테마 생성) 같은 이름의 예약은 생성 불가

--- src/test/java/roomescape/controller/exception/ThemeControllerExceptionTest.java
+++ src/test/java/roomescape/controller/exception/ThemeControllerExceptionTest.java
@@ -3,6 +3,7 @@ package roomescape.controller.exception;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import roomescape.dto.Theme;
 
 @DisplayName("예외 처리")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -18,10 +20,10 @@ import org.springframework.test.context.jdbc.Sql;
 @Sql("classpath:/test.sql")
 public class ThemeControllerExceptionTest {
 
-//    private static final String NAME_DATA1 = "워너고홈";
+    private static final String NAME_DATA1 = "워너고홈";
 //    private static final String NAME_DATA2 = "테스트 이름";
-//    private static final String DESC_DATA = "병맛 어드벤처 회사 코믹물";
-//    private static final int PRICE_DATA = 29000;
+    private static final String DESC_DATA = "병맛 어드벤처 회사 코믹물";
+    private static final int PRICE_DATA = 29000;
 
     private static final String THEME_PATH = "/theme";
     @LocalServerPort
@@ -46,5 +48,18 @@ public class ThemeControllerExceptionTest {
                 .when().post(THEME_PATH)
                 .then().log().all()
                 .statusCode(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value());
+    }
+
+    @DisplayName("테마 생성) 같은 이름의 예약은 생성 불가")
+    @Test
+    void failToCreateReservationAlreadyExist() {
+        Theme theme = new Theme(NAME_DATA1, DESC_DATA, PRICE_DATA);
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(theme)
+                .when().post(THEME_PATH)
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 }
