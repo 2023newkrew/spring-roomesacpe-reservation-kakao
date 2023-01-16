6013feae3785298b5f758d32da5fd7395d18eff5
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-01-16 13:10:05 +0900

    feat: ReservationService에 Reservation과 Theme 클래스 분리 관련 변경사항 반영

--- src/main/java/nextstep/service/ReservationService.java
+++ src/main/java/nextstep/service/ReservationService.java
@@ -1,7 +1,6 @@
 package nextstep.service;
 
 import nextstep.domain.Reservation;
-import nextstep.domain.Theme;
 import nextstep.repository.reservation.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,20 +19,19 @@ public class ReservationService {
     }
 
     public Reservation findById(Long id) {
-        Reservation reservation = reservationRepository.findById(id);
-        return reservation;
+        return reservationRepository.findById(id);
     }
 
     public void deleteReservation(Long id) {
         reservationRepository.deleteById(id);
     }
 
-    public Long createReservation(LocalDate date, LocalTime time, String name, Theme theme) {
-        return reservationRepository.save(date, time, name, theme);
+    public Long createReservation(LocalDate date, LocalTime time, String name, Long themeId) {
+        return reservationRepository.save(date, time, name, themeId);
     }
 
     public Long createReservation(Reservation reservation) {
         return this.createReservation(reservation.getDate(), reservation.getTime(),
-                reservation.getName(), reservation.getTheme());
+                reservation.getName(), reservation.getThemeId());
     }
 }
