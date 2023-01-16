045fcafef9f1026bcc973af4a1d9f836ec0fc24a
person003333 <person00333@gmail.com>
2023-01-16 11:06:33 +0900

    feat : Theme repository 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,18 @@
 CREATE TABLE reservation
 (
-    id          bigint not null auto_increment comment='시스템 식별번호',
-    date        date comment='예약일자',
-    time        time comment='예약시간',
-    name        varchar(20) comment='예약자 이름',
-    theme_name  varchar(20) comment='테마 이름',
-    theme_desc  varchar(255) comment='테마 소개',
-    theme_price int comment='테마 가격',
+    id          bigint not null auto_increment,
+    date     date ,
+    time     time ,
+    name        varchar(20),
+    theme_id bigint not null ,
     primary key (id)
-) comment='방탈출 예약자 정보';
+);
+
+CREATE TABLE theme
+(
+    id    bigint not null auto_increment,
+    name  varchar(20) ,
+    desc  varchar(255),
+    price int ,
+    primary key (id)
+);
--- src/test/java/nextstep/roomescape/theme/ThemeRepositoryTest.java
+++ src/test/java/nextstep/roomescape/theme/ThemeRepositoryTest.java
@@ -1,13 +1,10 @@
 package nextstep.roomescape.theme;
 
 import nextstep.roomescape.theme.domain.entity.Theme;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,16 +15,16 @@ class ThemeRepositoryTest {
     private ThemeRepository themeRepository;
 
 
-//    @Test
-//    @Transactional
-//    void create() {
-//        Theme theme = new Theme("워너고홈", "병맛 어드벤처 회사 코믹물", 29000);
-//        Theme createTheme = themeRepository.create(theme);
-//
-//        assertTrue(theme.getName().equals(createTheme.getName()) &&
-//                theme.getDesc().equals(createTheme.getDesc()) &&
-//                theme.getPrice().equals(createTheme.getPrice()));
-//    }
+    @Test
+    @Transactional
+    void create() {
+        Theme theme = new Theme(null,"워너고홈", "병맛 어드벤처 회사 코믹물", 29000);
+        Theme createTheme = themeRepository.create(theme);
+
+        assertTrue(theme.getName().equals(createTheme.getName()) &&
+                theme.getDesc().equals(createTheme.getDesc()) &&
+                theme.getPrice().equals(createTheme.getPrice()));
+    }
 
     @Test
     void findAll() {
