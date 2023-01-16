a979b2932a1855166222e66c719a05b03e41423d
gkwodn1044 <gkwodn1044@gmail.com>
2023-01-16 17:35:19 +0900

    feat : Theme 도메인 id 추가

--- src/main/java/nextstep/Theme.java
+++ src/main/java/nextstep/Theme.java
@@ -1,15 +1,19 @@
 package nextstep;
 
 public class Theme {
+    private Long id;
     private String name;
     private String desc;
     private Integer price;
 
-    public Theme(String name, String desc, Integer price) {
+    public Theme(Long id, String name, String desc, Integer price) {
+        this.id = id;
         this.name = name;
         this.desc = desc;
         this.price = price;
     }
+
+    public Long getId() { return id; }
 
     public String getName() {
         return name;
