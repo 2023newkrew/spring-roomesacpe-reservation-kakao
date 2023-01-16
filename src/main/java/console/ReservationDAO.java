05cde3e1dc6f04ea34a098e5254b822f958b2f02
HQkim <hyunkyu001@gmail.com>
2023-01-16 13:53:18 +0900

    fix: Connection을 try-with-resources 구문으로 변경하여 리소스 반납 보장

--- src/main/java/console/ReservationDAO.java
+++ src/main/java/console/ReservationDAO.java
@@ -1,31 +1,16 @@
 package console;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Time;
-import java.util.Optional;
 import web.domain.Reservation;
 import web.domain.Theme;
+
+import java.sql.*;
+import java.util.Optional;
 
 public class ReservationDAO {
 
     public void addReservation(Reservation reservation) {
-        Connection con = null;
-
-        // 드라이버 연결
-        try {
-            con = DriverManager.getConnection("jdbc:h2:~/test;AUTO_SERVER=true", "sa", "");
+        try(Connection con = DriverManager.getConnection("jdbc:h2:~/test;AUTO_SERVER=true", "sa", "")) {
             System.out.println("정상적으로 연결되었습니다.");
-        } catch (SQLException e) {
-            System.err.println("연결 오류:" + e.getMessage());
-            e.printStackTrace();
-        }
-
-        try {
             String sql = "INSERT INTO reservation (date, time, name, theme_name, theme_desc, theme_price) VALUES (?, ?, ?, ?, ?, ?);";
             PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
             ps.setDate(1, Date.valueOf(reservation.getDate()));
@@ -35,25 +20,20 @@ public class ReservationDAO {
             ps.setString(5, reservation.getTheme().getDesc());
             ps.setInt(6, reservation.getTheme().getPrice());
             ps.executeUpdate();
-            con.close();
         } catch (SQLException e) {
+            System.err.println("연결 오류:" + e.getMessage());
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
 
     public Optional<Reservation> findById(Long reservationId) {
-        Connection con = null;
+//        Connection con = null;
 
         // 드라이버 연결
-        try {
-            con = DriverManager.getConnection("jdbc:h2:~/test;AUTO_SERVER=true", "sa", "");
-            System.out.println("정상적으로 연결되었습니다.");
-        } catch (SQLException e) {
-            System.err.println("연결 오류:" + e.getMessage());
-            e.printStackTrace();
-        }
+        try(Connection con = DriverManager.getConnection("jdbc:h2:~/test;AUTO_SERVER=true", "sa", "")) {
 
-        try {
+            System.out.println("정상적으로 연결되었습니다.");
             String sql = "SELECT * FROM reservation WHERE id=?";
 
             PreparedStatement ps = con.prepareStatement(sql);
@@ -76,26 +56,18 @@ public class ReservationDAO {
                     theme
             );
 
-            con.close();
             return Optional.of(reservation);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.err.println("연결 오류:" + e.getMessage());
+            e.printStackTrace();
+            return Optional.empty();
         }
     }
 
     public boolean deleteById(Long reservationId) {
-        Connection con = null;
+        try(Connection con = DriverManager.getConnection("jdbc:h2:~/test;AUTO_SERVER=true", "sa", "")) {
 
-        // 드라이버 연결
-        try {
-            con = DriverManager.getConnection("jdbc:h2:~/test;AUTO_SERVER=true", "sa", "");
             System.out.println("정상적으로 연결되었습니다.");
-        } catch (SQLException e) {
-            System.err.println("연결 오류:" + e.getMessage());
-            e.printStackTrace();
-        }
-
-        try {
             String sql = "DELETE FROM reservation WHERE id=?";
 
             PreparedStatement ps = con.prepareStatement(sql);
@@ -106,6 +78,8 @@ public class ReservationDAO {
 
             return rowNum > 0;
         } catch (SQLException e) {
+            System.err.println("연결 오류:" + e.getMessage());
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
