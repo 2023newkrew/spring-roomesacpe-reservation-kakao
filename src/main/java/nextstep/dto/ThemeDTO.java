48eb57f04e05760217c750fad11935865bd9d612
gkwodn1044 <gkwodn1044@gmail.com>
2023-01-16 17:44:02 +0900

    feat : ThemeDTO 수정, ThemeRequestDTO 추가

--- src/main/java/nextstep/dto/ThemeDTO.java
+++ src/main/java/nextstep/dto/ThemeDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @AllArgsConstructor
 @Data
 public class ThemeDTO {
+    private Long id;
 
     private String name;
 
---
+++ src/main/java/nextstep/dto/ThemeRequestDTO.java
@@ -0,0 +1,12 @@
+package nextstep.dto;
+
+import lombok.Data;
+
+@Data
+public class ThemeRequestDTO {
+    private String name;
+
+    private String desc;
+
+    private int price;
+}
