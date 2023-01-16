c19485b11d52a55574367755fa6993d034df6b3b
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-01-17 02:43:18 +0900

    refactor: ReservationResponse JSON 반환 형태 변경, 이를 위한 Custom Serializer 클래스 추가
    - 과제 스펙대로 반환 실패
    - unwrap이 안되는 상태

--- src/main/java/nextstep/reservation/dto/ReservationResponse.java
+++ src/main/java/nextstep/reservation/dto/ReservationResponse.java
@@ -1,10 +1,13 @@
 package nextstep.reservation.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import nextstep.reservation.entity.Reservation;
+import nextstep.reservation.serializer.ThemeResponseSerializer;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -15,9 +18,11 @@ import java.time.LocalTime;
 public class ReservationResponse {
     private final long id;
     private final LocalDate date;
-    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern = "kk:mm")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "kk:mm")
     private final LocalTime time;
     private final String name;
+    @JsonSerialize(using = ThemeResponseSerializer.class)
+    @JsonUnwrapped
     private final ThemeResponse themeResponse;
 
     public static ReservationResponse from(Reservation reservation, ThemeResponse themeResponse) {
