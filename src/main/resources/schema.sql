66c563d45f46cd5ef381c921925b1898fe2fef94
person003333 <person00333@gmail.com>
2023-01-17 16:32:43 +0900

    refactor : 테이블 comment 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,18 @@
 CREATE TABLE reservation
 (
-    id          bigint not null auto_increment,
-    date     date ,
-    time     time ,
-    name        varchar(20),
-    theme_id bigint not null ,
+    id          bigint not null auto_increment comment '예약ID',
+    date     date comment '예약날짜',
+    time     time comment '예약시간',
+    name        varchar(20) comment '예약자이름',
+    theme_id bigint not null comment '테마ID',
     primary key (id)
 );
 
 CREATE TABLE theme
 (
-    id    bigint not null auto_increment,
-    name  varchar(20) ,
-    desc  varchar(255),
-    price int ,
+    id    bigint not null auto_increment comment '테마ID',
+    name  varchar(20) comment '테마이름',
+    desc  varchar(255) comment '테마설명',
+    price int comment '테마가격',
     primary key (id)
 );
