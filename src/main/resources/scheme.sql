27b8dd08bea42d030816e5e7cedb81e0a352a2cc
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-01-18 00:22:16 +0900

    chore: schema.sql에 항목 코멘트 추가

--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS THEME CASCADE;
 
 CREATE TABLE THEME
 (
-    id          bigint not null auto_increment,
-    name        varchar(20),
-    desc        varchar(255),
-    price       int,
+    id          bigint not null auto_increment  comment '테마 ID',
+    name        varchar(20)                     comment '테마 이름',
+    desc        varchar(255)                    comment '테마 설명',
+    price       int                             comment '테마 가격',
     primary key (id)
 );
 
@@ -13,10 +13,10 @@ DROP TABLE IF EXISTS RESERVATION CASCADE;
 
 CREATE TABLE RESERVATION
 (
-    id          bigint not null auto_increment,
-    date        date,
-    time        time,
-    name        varchar(20),
-    theme_id    bigint,
+    id          bigint not null auto_increment  comment '예약 ID',
+    date        date                            comment '예약일',
+    time        time                            comment '예약시간',
+    name        varchar(20)                     comment '예약자명',
+    theme_id    bigint                          comment '테마 ID',
     primary key (id)
 );
