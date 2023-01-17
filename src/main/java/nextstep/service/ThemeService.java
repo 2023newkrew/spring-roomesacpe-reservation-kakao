5a208361d50cf5241c40c75b22f13324c9a20648
Siryeong <siryeongchoi@gmail.com>
2023-01-17 18:09:33 +0900

    chore: print코드 삭제

--- src/main/java/nextstep/service/ThemeService.java
+++ src/main/java/nextstep/service/ThemeService.java
@@ -47,7 +47,6 @@ public class ThemeService {
     }
 
     public boolean deleteThemeById(Long id) {
-        System.out.println(id);
         if(reservationRepository.findByThemeId(id).size() > 0)
             throw new ReservedThemeDeleteException();
 
