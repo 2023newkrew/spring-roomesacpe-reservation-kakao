1554862f18a7d43c98c15dd70d8b67b23f9f91cd
BaeRoNuI <xogh20321@gmail.com>
2023-01-18 00:16:08 +0900

    feat: add exception cases

--- src/main/java/kakao/exception/ErrorCode.java
+++ src/main/java/kakao/exception/ErrorCode.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "Reservation not found."),
-    DUPLICATE_RESERVATION(HttpStatus.CONFLICT, "Duplicated reservation.");
+    THEME_NOT_FOUND(HttpStatus.NOT_FOUND, "Theme not found."),
+    DUPLICATE_RESERVATION(HttpStatus.CONFLICT, "Duplicated reservation."),
+    DUPLICATE_THEME(HttpStatus.CONFLICT, "Duplicated theme."),
+    CORRESPONDING_RESERVATION_EXIST(HttpStatus.CONFLICT, "Corresponding reservation exist. Could not remove theme");
 
     private final HttpStatus httpStatus;
     private final String message;
