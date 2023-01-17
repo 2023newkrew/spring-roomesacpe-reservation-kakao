d81f644f985ca2c6e735c234f7453eff66b6c4f0
abcworld123 <rkaxhdals@gmail.com>
2023-01-17 16:03:16 +0900

    fix: db init 버그 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS reservation
     datetime datetime,
     name     varchar(20),
     theme_id bigint not null,
-    primary key (id)
+    primary key (id),
+    unique (datetime)
 );
 
 CREATE TABLE IF NOT EXISTS theme
@@ -13,11 +14,9 @@ CREATE TABLE IF NOT EXISTS theme
     name  varchar(20),
     desc  varchar(255),
     price int,
-    primary key (id)
+    primary key (id),
+    unique (name)
 );
 
 CREATE INDEX ON reservation(datetime);
 CREATE INDEX ON theme(name);
-
-alter table reservation add constraint datetime_unique unique(datetime);
-alter table theme add constraint name_unique unique(name);
