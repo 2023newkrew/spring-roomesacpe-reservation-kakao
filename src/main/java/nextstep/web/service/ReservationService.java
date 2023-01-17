5fdb34bcc8b459399b74603743ba162a678b0b59
Siryeong <siryeongchoi@gmail.com>
2023-01-17 22:05:09 +0900

    refactor: service 에서 Manager를 호출하도록 리팩터링

--- src/main/java/nextstep/web/service/ReservationService.java
+++ src/main/java/nextstep/web/service/ReservationService.java
@@ -1,64 +1,30 @@
 package nextstep.web.service;
 
-import nextstep.domain.Reservation;
-import nextstep.domain.Theme;
+import nextstep.domain.ReservationManager;
 import nextstep.domain.repository.ReservationRepository;
 import nextstep.domain.repository.ThemeRepository;
 import nextstep.dto.CreateReservationRequest;
 import nextstep.dto.FindReservationResponse;
-import nextstep.exception.DuplicateReservationException;
-import nextstep.exception.ReservationNotFoundException;
-import nextstep.exception.ThemeNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 @Service
 public class ReservationService {
 
-    private static final Theme THEME = new Theme("워너고홈", "병맛 어드벤처 회사 코믹물", 29_000);
-
-    private final ReservationRepository reservationRepository;
-    private final ThemeRepository themeRepository;
+    private final ReservationManager reservationManager;
 
     public ReservationService(ReservationRepository reservationRepository, ThemeRepository themeRepository) {
-        this.reservationRepository = reservationRepository;
-        this.themeRepository = themeRepository;
+        this.reservationManager = new ReservationManager(reservationRepository, themeRepository);
     }
 
     public Long createReservation(CreateReservationRequest createReservationRequest) {
-        LocalDate date = LocalDate.parse(createReservationRequest.getDate());
-        LocalTime time = LocalTime.parse(createReservationRequest.getTime());
-
-        if (reservationRepository.existsByDateAndTime(date, time)) {
-            throw new DuplicateReservationException();
-        }
-        if(themeRepository.findThemeById(createReservationRequest.getThemeId()).isEmpty()) {
-            throw new ThemeNotFoundException();
-        }
-
-        Reservation savedReservation = reservationRepository.save(
-                new Reservation(
-                        date,
-                        time,
-                        createReservationRequest.getName(),
-                        createReservationRequest.getThemeId()
-                )
-        );
-        return savedReservation.getId();
+        return reservationManager.createReservation(createReservationRequest);
     }
 
     public FindReservationResponse findReservationById(Long reservationId) {
-        Reservation reservation = reservationRepository.findById(reservationId)
-                .orElseThrow(ReservationNotFoundException::new);
-        Theme theme = themeRepository.findThemeById(reservation.getThemeId())
-                .orElseThrow(ThemeNotFoundException::new);
-
-        return FindReservationResponse.from(reservation, theme);
+        return reservationManager.findReservationById(reservationId);
     }
 
     public boolean deleteReservationById(Long reservationId) {
-        return reservationRepository.deleteById(reservationId);
+        return reservationManager.deleteReservationById(reservationId);
     }
 }
--- src/main/java/nextstep/web/service/ThemeService.java
+++ src/main/java/nextstep/web/service/ThemeService.java
@@ -1,55 +1,35 @@
 package nextstep.web.service;
 
-import nextstep.domain.Theme;
+import nextstep.domain.ThemeManager;
 import nextstep.domain.repository.ReservationRepository;
 import nextstep.domain.repository.ThemeRepository;
 import nextstep.dto.CreateThemeRequest;
 import nextstep.dto.ThemeResponse;
 import nextstep.dto.ThemesResponse;
-import nextstep.exception.DuplicateThemeException;
-import nextstep.exception.ReservedThemeDeleteException;
-import nextstep.exception.ThemeNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class ThemeService {
 
-    private final ThemeRepository themeRepository;
-    private final ReservationRepository reservationRepository;
+    private final ThemeManager themeManager;
 
     public ThemeService(ThemeRepository themeRepository, ReservationRepository reservationRepository){
-        this.themeRepository = themeRepository;
-        this.reservationRepository = reservationRepository;
+        themeManager = new ThemeManager(reservationRepository, themeRepository);
     }
 
     public Long createTheme(CreateThemeRequest themeRequest){
-        if(themeRepository.existByThemeName(themeRequest.getName()))
-            throw new DuplicateThemeException();
-
-        return themeRepository.save(new Theme(themeRequest.getName(), themeRequest.getDesc(), themeRequest.getPrice()));
+        return themeManager.createTheme(themeRequest);
     }
 
     public ThemeResponse findThemeById(Long id) {
-        Theme theme = themeRepository.findThemeById(id).orElseThrow(ThemeNotFoundException::new);
-        return ThemeResponse.from(theme);
+        return themeManager.findThemeById(id);
     }
 
     public ThemesResponse getAllThemes() {
-        List<Theme> themes = themeRepository.getAllThemes();
-        return new ThemesResponse(
-                themes.stream()
-                        .map(theme -> ThemeResponse.from(theme))
-                        .collect(Collectors.toList())
-        );
+        return themeManager.getAllThemes();
     }
 
     public boolean deleteThemeById(Long id) {
-        if(reservationRepository.findByThemeId(id).size() > 0)
-            throw new ReservedThemeDeleteException();
-
-        return themeRepository.deleteThemeById(id);
+        return themeManager.deleteThemeById(id);
     }
 }
