1ac6c2af605bb42b97c4e9177d6dc101766de11d
Subin Min <tnqls5417@naver.com>
2023-01-17 15:32:16 +0900

    refactor: SQL COMMENT 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS RESERVATION
 (
-    id          bigint not null auto_increment, -- 방탈출 예약 번호
-    date        date, -- 방탈출 예약 날짜
-    time        time, -- 방탈출 예약 시간
-    name        varchar(20), -- 방탈출 예약 이름
-    theme_id bigint not null, -- 예약된 방탈출 테마 아이디
+    id          bigint not null auto_increment COMMENT '방탈출 예약 번호',
+    date        date COMMENT '방탈출 예약 날짜',
+    time        time COMMENT '방탈출 예약 시간',
+    name        varchar(20) COMMENT '방탈출 예약 이름',
+    theme_id bigint not null COMMENT '예약된 방탈출 테마 번호',
     primary key (id)
 );
 
 CREATE TABLE IF NOT EXISTS THEME
 (
-    id    bigint not null auto_increment, -- 방탈출 테마 번호
-    name  varchar(20), -- 방탈출 테마 이름
-    desc  varchar(255), -- 방탈출 테마 설명
-    price int, -- 방탈출 테마 가격
+    id    bigint not null auto_increment COMMENT '방탈출 테마 번호',
+    name  varchar(20) COMMENT '방탈출 테마 이름',
+    desc  varchar(255) COMMENT '방탈출 테마 설명',
+    price int COMMENT '방탈출 테마 가격',
     primary key (id)
 );
