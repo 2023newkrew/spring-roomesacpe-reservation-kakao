bf108940bb02612c2d91fb41cda125020846e224
BaeRoNuI <xogh20321@gmail.com>
2023-01-18 00:12:51 +0900

    feat: extract service related with theme

--- src/main/java/kakao/service/ReservationServiceImpl.java
+++ src/main/java/kakao/service/ReservationServiceImpl.java
@@ -4,35 +4,49 @@ import kakao.exception.DuplicatedReservationException;
 import kakao.exception.ReservationNotFoundException;
 import kakao.controller.request.ReservationRequest;
 import kakao.controller.response.ReservationResponse;
+import kakao.exception.ThemeNotFoundException;
+import kakao.model.Reservation;
+import kakao.model.Theme;
 import kakao.repository.ReservationRepository;
 import kakao.repository.ThemeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ReservationServiceImpl implements ReservationService{
 
     private final ReservationRepository reservationRepository;
     private final ThemeRepository themeRepository;
 
-    public ReservationServiceImpl(ReservationRepository reservationRepository, ThemeRepository themeRepository) {
-        this.reservationRepository = reservationRepository;
-        this.themeRepository = themeRepository;
-    }
-
     public Long book(ReservationRequest reservationRequest) {
-        if (reservationRepository.findByDateAndTime(reservationRequest.getDate(), reservationRequest.getTime()).isEmpty()) {
-            throw new DuplicatedReservationException();
-        }
+        checkIfThemeExist(reservationRequest.getThemeId());
+        checkIfDuplicatedReservation(reservationRequest);
 
-        return reservationRepository.create(reservationRequest, ThemeRepository.DEFAULT_THEME);
+        return reservationRepository.create(reservationRequest);
     }
 
     public ReservationResponse lookUp(Long id) {
-        return reservationRepository.findById(id)
+        Reservation reservation = reservationRepository.findById(id)
                 .orElseThrow(ReservationNotFoundException::new);
+        Theme theme = themeRepository.findById(reservation.getThemeId())
+                .orElseThrow(ThemeNotFoundException::new);
+
+        return new ReservationResponse(reservation, theme);
     }
 
     public void cancel(Long id) {
         reservationRepository.deleteById(id);
+    }
+
+    private void checkIfThemeExist(Long themeId) {
+        if(themeRepository.findById(themeId).isEmpty()) {
+            throw new ThemeNotFoundException();
+        }
+    }
+    private void checkIfDuplicatedReservation(ReservationRequest reservationRequest) {
+        if (reservationRepository.findByDateAndTimeAndThemeId(reservationRequest.getDate(), reservationRequest.getTime(), reservationRequest.getThemeId()).isPresent()) {
+            throw new DuplicatedReservationException();
+        }
     }
 }
