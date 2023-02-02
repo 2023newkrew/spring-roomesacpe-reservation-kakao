08b445a59ff55ace086ef5d4e733d293cf61fa54
gkwodn1044 <gkwodn1044@gmail.com>
2023-02-02 15:45:48 +0900

    feat : JdbcThemeDAO DAOImple 패키지 아래로 이동, 메소드에서 DTO가 아닌 Entity를 사용하도록 변경, 불필요한 메소드 분리 제거, getById 메소드 구현

--- src/main/java/nextstep/dao/DAOImple/JdbcThemeDAO.java
+++ src/main/java/nextstep/dao/DAOImple/JdbcThemeDAO.java
@@ -1,6 +1,8 @@
-package nextstep.dao;
+package nextstep.dao.DAOImple;
 
 import lombok.AllArgsConstructor;
+import nextstep.dao.ThemeDAO;
+import nextstep.domain.Theme;
 import nextstep.dto.ReservationDTO;
 import nextstep.dto.ThemeDTO;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,17 +13,19 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Time;
 import java.util.List;
 import java.util.Objects;
 
 @AllArgsConstructor
 @Component
 public class JdbcThemeDAO implements ThemeDAO {
-    private static final RowMapper<ThemeDTO> THEME_DTO_ROW_MAPPER =
+    private static final RowMapper<Theme> THEME_ROW_MAPPER =
             (resultSet, rowNum) -> {
-                return new ThemeDTO(
+                return new Theme(
                         resultSet.getLong("id"),
                         resultSet.getString("name"),
                         resultSet.getString("desc"),
@@ -31,35 +35,36 @@ public class JdbcThemeDAO implements ThemeDAO {
     private final JdbcTemplate jdbcTemplate;
 
     @Override
-    public Long insert(ThemeDTO dto) {
+    public Long insert(Theme theme) {
         KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(getPreparedStatementCreator(dto), keyHolder);
-        Number key = keyHolder.getKey();
 
-        return Objects.requireNonNull(key)
-                .longValue();
-    }
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(INSERT_SQL, new String[]{"id"});
+            ps.setString(1, theme.getName());
+            ps.setString(2, theme.getDesc());
+            ps.setInt(3, theme.getPrice());
+            return ps;
 
-    private PreparedStatementCreator getPreparedStatementCreator(ThemeDTO dto) {
-        return connection -> getPrepareStatement(connection, dto);
-    }
+        }, keyHolder);
 
-    private PreparedStatement getPrepareStatement(Connection connection, ThemeDTO dto) throws SQLException {
-        var ps = connection.prepareStatement(INSERT_SQL, new String[]{"id"});
-
-        ps.setString(1, dto.getName());
-        ps.setString(2, dto.getDesc());
-        ps.setInt(3, dto.getPrice());
-
-        return ps;
+        return keyHolder.getKey().longValue();
     }
 
     @Override
-    public List<ThemeDTO> getList() {
+    public List<Theme> getList() {
         try {
-            return jdbcTemplate.query(SELECT_SQL, THEME_DTO_ROW_MAPPER);
+            return jdbcTemplate.query(SELECT_SQL, THEME_ROW_MAPPER);
         }
         catch (Exception ignore) {
+            return null;
+        }
+    }
+
+    @Override
+    public Theme getById(Long id) {
+        try {
+            return jdbcTemplate.queryForObject(SELECT_BY_ID_SQL, THEME_ROW_MAPPER, id);
+        } catch (Exception e) {
             return null;
         }
     }
