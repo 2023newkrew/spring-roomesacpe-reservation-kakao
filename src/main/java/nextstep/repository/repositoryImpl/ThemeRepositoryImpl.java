d3b162495ab97a7e051499f74b1634e434351f99
gkwodn1044 <gkwodn1044@gmail.com>
2023-02-02 15:50:22 +0900

    feat : ThemeRepositoryImpl repositoryImpl 패키지 아래로 이동, getById 메소드 구현

--- src/main/java/nextstep/repository/repositoryImpl/ThemeRepositoryImpl.java
+++ src/main/java/nextstep/repository/repositoryImpl/ThemeRepositoryImpl.java
@@ -1,14 +1,12 @@
-package nextstep.repository;
+package nextstep.repository.repositoryImpl;
 
 import lombok.AllArgsConstructor;
 import nextstep.domain.Theme;
 import nextstep.dao.ThemeDAO;
-import nextstep.dto.ThemeDTO;
-import nextstep.mapper.ThemeMapper;
+import nextstep.repository.ThemeRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Repository
@@ -17,18 +15,17 @@ public class ThemeRepositoryImpl implements ThemeRepository {
 
     @Override
     public Long insert(Theme theme) {
-        ThemeDTO dto = ThemeMapper.INSTANCE.toDto(theme);
-
-        return dao.insert(dto);
+        return dao.insert(theme);
     }
 
     @Override
     public List<Theme> getList() {
-        List<Theme> list = dao.getList().stream()
-                .map(dto -> ThemeMapper.INSTANCE.fromDto(dto))
-                .collect(Collectors.toList());
+        return dao.getList();
+    }
 
-        return list;
+    @Override
+    public Theme getById(Long id) {
+        return dao.getById(id);
     }
 
     @Override
