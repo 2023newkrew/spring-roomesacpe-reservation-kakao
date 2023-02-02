a857a869678fe187d7e5ec37055c4f9722b39b7b
gkwodn1044 <gkwodn1044@gmail.com>
2023-02-02 15:55:36 +0900

    feat : ReservationController createReservation 메소드 내 try-catch문 제거, 메소드 반환값의 제네릭 타입 변경

--- src/main/java/nextstep/controller/ReservationController.java
+++ src/main/java/nextstep/controller/ReservationController.java
@@ -1,8 +1,8 @@
 package nextstep.controller;
 
 import lombok.RequiredArgsConstructor;
-import nextstep.domain.Reservation;
 import nextstep.dto.ReservationRequestDTO;
+import nextstep.dto.ReservationResponseDTO;
 import nextstep.service.ReservationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,31 +13,23 @@ import java.net.URI;
 @RequestMapping("/reservations")
 @RestController
 public class ReservationController {
-
     private static final String RESERVATION_PATH = "/reservations";
-
     private final ReservationService service;
 
     @PostMapping
-    public ResponseEntity<Exception> createReservation(@RequestBody ReservationRequestDTO request) {
-        try {
-            URI location = URI.create(RESERVATION_PATH + "/" + service.create(request));
+    public ResponseEntity createReservation(@RequestBody ReservationRequestDTO request) {
+        URI location = URI.create(RESERVATION_PATH + "/" + service.create(request));
 
-            return ResponseEntity.created(location)
-                    .build();
-        } catch(Exception e) {
-            return ResponseEntity.badRequest().body(e);
-        }
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping("/{reservation_id}")
-    public ResponseEntity<Reservation> getReservation(@PathVariable("reservation_id") Long reservationId) {
-
+    public ResponseEntity<ReservationResponseDTO> getReservation(@PathVariable("reservation_id") Long reservationId) {
         return ResponseEntity.ok(service.getById(reservationId));
     }
 
     @DeleteMapping("/{reservation_id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable("reservation_id") Long reservationId) {
+    public ResponseEntity deleteReservation(@PathVariable("reservation_id") Long reservationId) {
         service.deleteById(reservationId);
 
         return ResponseEntity.noContent()
