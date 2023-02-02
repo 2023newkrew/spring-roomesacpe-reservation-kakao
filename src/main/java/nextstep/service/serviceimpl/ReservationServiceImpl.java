aad4b59d82540398d14681ec8627bb1007b26133
gkwodn1044 <gkwodn1044@gmail.com>
2023-02-02 15:53:38 +0900

    feat : ReservationServiceImpl create 메소드에 date, time 검증 로직 및 테마 번호 검증 로직 추가, getById 메소드 예약 번호 검증 로직 추가

--- src/main/java/nextstep/service/serviceimpl/ReservationServiceImpl.java
+++ src/main/java/nextstep/service/serviceimpl/ReservationServiceImpl.java
@@ -2,19 +2,22 @@ package nextstep.service.serviceimpl;
 
 import lombok.RequiredArgsConstructor;
 import nextstep.domain.Reservation;
+import nextstep.domain.Theme;
 import nextstep.dto.ReservationRequestDTO;
+import nextstep.dto.ReservationResponseDTO;
 import nextstep.repository.ReservationRepository;
+import nextstep.repository.ThemeRepository;
 import nextstep.service.ReservationService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Objects;
 
 @RequiredArgsConstructor
 @Service
 public class ReservationServiceImpl implements ReservationService {
     private final ReservationRepository repository;
+    private final ThemeRepository themeRepository;
 
     @Override
     public Long create(ReservationRequestDTO request) {
@@ -22,18 +25,34 @@ public class ReservationServiceImpl implements ReservationService {
         LocalTime time = LocalTime.parse(request.getTime() + ":00");
         String name = request.getName();
         Long theme_id = request.getTheme_id();
-        Reservation reservation = new Reservation(null, date, time, name, theme_id);
-        Long id = repository.insertIfNotExistsDateTime(reservation);
-        if(Objects.isNull(id)){
-            throw new IllegalArgumentException("해당 시간에 이미 예약이 존재합니다.");
+
+        if (repository.existsByDateTime(date, time)) {
+            throw new IllegalArgumentException("해당 시간에 이미 예약이 존재합니다");
         }
+
+        Theme theme = themeRepository.getById(theme_id);
+
+        if (theme == null) {
+            throw new IllegalArgumentException("존재하지 않는 테마입니다");
+        }
+
+        Reservation reservation = new Reservation(date, time, name, theme_id);
+        Long id = repository.insert(reservation);
 
         return id;
     }
 
     @Override
-    public Reservation getById(Long id) {
-        return repository.getById(id);
+    public ReservationResponseDTO getById(Long id) {
+        Reservation reservation = repository.getById(id);
+
+        if (reservation == null) {
+            throw new IllegalArgumentException("잘못된 예약 번호입니다");
+        }
+
+        Theme theme = themeRepository.getById(reservation.getTheme_id());
+
+        return new ReservationResponseDTO(reservation, theme);
     }
 
     @Override
