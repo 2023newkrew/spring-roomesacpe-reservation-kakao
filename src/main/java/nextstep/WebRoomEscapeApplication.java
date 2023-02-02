5b14fe06273f71f89ff4a370a4dce2f1699ece22
gkwodn1044 <gkwodn1044@gmail.com>
2023-02-02 16:02:06 +0900

    feat : 콘솔 어플리케이션 theme 추가/조회/삭제 기능 추가, reservation 추가/조회시 테마 정보 추가, reservation 추가/조회시 예외처리, 인메모리 h2db 사용을 위한 db 초기화 메소드 추가

--- src/main/java/nextstep/WebRoomEscapeApplication.java
+++ src/main/java/nextstep/WebRoomEscapeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebRoomEscapeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WebRoomEscapeApplication.class, args);
     }
